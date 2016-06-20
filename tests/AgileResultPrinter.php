0749cb17c4deae9b8937a86d7c75712bc0375def
Romans Malinovskis <romans@agiletoolkit.org>
2016-06-20 14:21:35 +0100

    Make use of Exception::getColorfulText() - display colorful exceptions in PHPUnit

---
+++ tests/AgileResultPrinter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace atk4\data\tests;
+
+class AgileResultPrinter extends  \PHPUnit_TextUI_ResultPrinter {
+
+    protected function printDefectTrace(\PHPUnit_Framework_TestFailure $defect)
+    {
+        $e = $defect->thrownException();
+        if (!$e instanceof AgileExceptionWrapper) {
+            return parent::printDefectTrace($defect);
+        }
+        $this->write((string) $e);
+
+        $p = $e->getPrevious();
+
+        if (
+            $p instanceof \atk4\core\Exception or 
+            $p instanceof \atk4\dsql\Exception) {
+            $this->write($p->getColorfulText());
+        }
+    }
+}
