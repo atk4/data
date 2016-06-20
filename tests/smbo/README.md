6949e7b6a57f3bd7afe89a431624a46404cbfc11
Romans Malinovskis <romans@agiletoolkit.org>
2016-06-20 14:30:55 +0100

    Added some extra tests donated from SMBO

---
+++ tests/smbo/README.md
@@ -0,0 +1,5 @@
+
+www.sortmybooks.com is a company that offers on-line accounting
+software package for small and medium companies. They have 
+contributed some database structured that is used in their
+application along model definitions and some unit-tests.
---
+++ tests/smbo/SMBOTest.php
@@ -0,0 +1,87 @@
+<?php
+namespace atk4\data\tests\smbo;
+
+use atk4\data\Model;
+use atk4\data\Persistence;
+use atk4\data\Field;
+
+class SMBOTest extends \atk4\data\tests\TestCase
+{
+
+
+    /**
+     * Test constructor
+     *
+     */
+    public function testBasicEntities()
+    {
+        $db = Persistence::connect($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
+
+        // Create a new company
+        $company = new Model_Company($db);
+        $company->set([
+            'name'=>'Test Company 1',
+            'director_name'=>'Tester Little', 
+            'type'=>'Limited Company',
+            'vat_registered'=>true
+        ]);
+        $company->save();
+        return;
+
+        // Create two new clients, one is sole trader, other is limited company
+        $client = $company->ref('Client');
+        list($john_id, $agile_id) = $m->insert([
+            ['name'=>'John Smith Consulting','vat_registered'=>false],
+            'Agile Software Limited',
+        ]);
+
+        // Insert a first, default invoice for our sole-trader
+        $john = $company->load($john_id);
+        $john_invoices = $john->ref('Invoice');
+        $john_invoices->insertInvoice([
+            'ref_no'=>'INV1',
+            'due_date'=>(new Date())->add(new DateInterval('2w')), // due in 2 weeks
+            'lines'=>[
+                ['descr'=>'Sold some sweets', 'total_gross'=>100.00],
+                ['descr'=>'Delivery', 'total_gross'=>10.00]
+            ]
+        ]);
+
+        // Use custom method to create a sub-nominal
+        $company->ref('Nominal')->insertSubNominal('Sales', 'Discounted');
+
+        // Insert our second invoice using set referencing
+        $company->ref('Client')->id($agile_id)->refSet('Invoice')->insertInvoice([
+            'lines'=>[
+                [
+                    'item_id'=>$john->ref('Product')->insert('Cat Food'),
+                    'nominal'=>'Sales:Discounted',
+                    'total_net'=>50.00,
+                    'vat_rate'=>23
+                    // calculates total_gross at 61.50.
+                ],
+                [
+                    'item_id'=>$john->ref('Service')->insert('Delivery'),
+                    'total_net'=>10.00,
+                    'vat_rate'=>'23%'
+                    // calculates total_gross at 12.30
+                ]
+            ]
+        ]);
+
+        // Next we create bank account
+        $hsbc = $john->ref('Account')->set('name','HSBC')->save();
+
+        // And each of our invoices will have one new payment
+        foreach($john_invoices as $invoice) {
+            $invoice->ref('Payment')->insert(['amount'=>10.20, 'bank_account_id'=>$hsbc]);
+        }
+
+        // Now let's execute report
+        $debt = $john->add('Model_Report_Debtors');
+
+        // This should give us total amount owed by all clients: 
+        // (100.00+10.00) + (61.50 + 12.30) - 10.20*2
+        $this->assertEquals(163.40, $debt->sum('amount'));
+    }
+}
