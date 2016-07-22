f2314f20d1482452912018c331331046c5e24af7
Imants Horsts <imants.horsts@inbox.lv>
2016-07-22 15:58:52 +0300

    simplify

--- src/Field_SQL.php
+++ src/Field_SQL.php
@@ -31,30 +31,19 @@ class Field_SQL extends Field implements \atk4\dsql\Expressionable
     public function getDSQLExpression($expression)
     {
         if (isset($this->owner->persistence_data['use_table_prefixes'])) {
-            if ($this->actual) {
-                return $expression->expr('{}.{}', [
-                    $this->join ? (isset($this->join->foreign_alias) ? $this->join->foreign_alias : $this->join->short_name)
+            return $expression->expr('{}.{}', [
+                $this->join
+                    ? (isset($this->join->foreign_alias)
+                        ? $this->join->foreign_alias
+                        : $this->join->short_name)
                     : ($this->owner->table_alias ?: $this->owner->table),
-                    $this->actual,
-                ]);
-            } else {
-                return $expression->expr('{}.{}', [
-                    $this->join ? (isset($this->join->foreign_alias) ? $this->join->foreign_alias : $this->join->short_name)
-                    : (isset($this->owner->table_alias) ? $this->owner->table_alias : $this->owner->table),
-                    $this->short_name,
-                ]);
-            }
+                $this->actual ?: $this->short_name,
+            ]);
         } else {
             // relations set flag use_table_prefixes, so no need to check them here
-            if ($this->actual) {
-                return $expression->expr('{}', [
-                    $this->actual,
-                ]);
-            } else {
-                return $expression->expr('{}', [
-                    $this->short_name,
-                ]);
-            }
+            return $expression->expr('{}', [
+                $this->actual ?: $this->short_name,
+            ]);
         }
     }
 }
