36d0978d83ea3987ef5515aacbc7c4cba9f5ad91
DarkSide <imants.horsts@inbox.lv>
2016-08-13 20:36:00 +0300

    comment should be above line

--- src/Join_SQL.php
+++ src/Join_SQL.php
@@ -9,10 +9,10 @@ namespace atk4\data;
  */
 class Join_SQL extends Join implements \atk4\dsql\Expressionable
 {
-    public $foreign_alias;
     /**
      * A short symbolic name that will be used as an alias for the joined table.
      */
+    public $foreign_alias;
 
     /**
      * By default this will be either "inner" (for strong) or "left" for weak joins.
