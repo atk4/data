5a54f24d03b450da66b5cded5e126d746008a282
Romans Malinovskis <romans@agiletoolkit.org>
2016-10-02 13:32:21 +0100

    Removing this line does not break the test

--- src/Model.php
+++ src/Model.php
@@ -710,7 +710,6 @@ class Model implements \ArrayAccess, \IteratorAggregate
 
         if ($f) {
             $f->system = true;
-            $f->ui['editable'] = false;
             if ($operator === '=' || func_num_args() == 2) {
                 $v = $operator === '=' ? $value : $operator;
 
