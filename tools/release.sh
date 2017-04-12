7463ed010a204a114008390996c6da8fc542a6f7
Romans Malinovskis <romans@agiletoolkit.org>
2017-04-12 11:29:08 +0100

    update release script

--- tools/release.sh
+++ tools/release.sh
@@ -50,8 +50,7 @@ done
 open "https://github.com/atk4/$product/compare/$prev_version...develop"
 
 # Update dependency versions
-composer require atk4/core
-composer require atk4/dsql
+composer require atk4/core atk4/dsql
 
 composer update
 ./vendor/phpunit/phpunit/phpunit  --no-coverage
