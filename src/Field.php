182f61ac5295af524e13d59a8e25b2c9f545aac8
Romans Malinovskis <me@nearly.guru>
2017-09-12 17:03:53 +0100

    Feature/fix core 1.3 (#248)
    * fix for Agile core 1.3
    
    * Apply fixes from StyleCI

--- src/Field.php
+++ src/Field.php
@@ -4,6 +4,7 @@
 
 namespace atk4\data;
 
+use atk4\core\DIContainerTrait;
 use atk4\core\TrackableTrait;
 
 /**
@@ -12,6 +13,7 @@ use atk4\core\TrackableTrait;
 class Field
 {
     use TrackableTrait;
+    use DIContainerTrait;
 
     // {{{ Properties
 
