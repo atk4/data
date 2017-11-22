ea60c8b73b9328063974da8d1baafaf458af081a
Imants Horsts <DarkSide666@users.noreply.github.com>
2017-11-22 14:26:15 +0200

    fix addTitle field visibility in case our_field is exclusively set

--- src/Reference_SQL_One.php
+++ src/Reference_SQL_One.php
@@ -177,8 +177,8 @@ class Reference_SQL_One extends Reference_One
         }, null, 20);
 
         // Set ID field as not visible in grid by default
-        if (!isset($this->owner->getElement($this->link)->ui['visible'])) {
-            $this->owner->getElement($this->link)->ui['visible'] = false;
+        if (!isset($this->owner->getElement($this->our_field)->ui['visible'])) {
+            $this->owner->getElement($this->our_field)->ui['visible'] = false;
         }
 
         return $ex;
