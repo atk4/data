3b4cf90247c10d028eb6e2c2a08da50f00283e0f
Imants Horsts <DarkSide666@users.noreply.github.com>
2017-12-17 02:13:46 +0200

    one more test

--- tests/StaticPersistenceTest.php
+++ tests/StaticPersistenceTest.php
@@ -13,10 +13,15 @@ class StaticPersistenceTest extends TestCase
     public function testBasicStatic()
     {
         $p = new \atk4\data\Persistence_Static(['hello', 'world']);
+
+        // default title field
         $m = new \atk4\data\Model($p);
-
         $m->load(1);
-
         $this->assertEquals('world', $m['name']);
+
+        // custom title field
+        $m = new \atk4\data\Model($p, ['title_field' => 'foo']);
+        $m->load(1);
+        $this->assertEquals('world', $m['name']); // still 'name' here not 'foo'
     }
 }
