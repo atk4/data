e3845a6c4c6d1d66556ef1b0a41e7a20aacc069a
Imants Horsts <DarkSide666@users.noreply.github.com>
2017-12-27 14:35:12 +0200

    add tests for field types

--- tests/StaticPersistenceTest.php
+++ tests/StaticPersistenceTest.php
@@ -93,6 +93,32 @@ class StaticPersistenceTest extends TestCase
         $m = new \atk4\data\Model($p);
         $this->assertEquals('title', $m->title_field);
     }
+    
+    public function testFieldTypes()
+    {
+        $p = new \atk4\data\Persistence_Static([[
+            'name' => 'hello',
+            'test_int'    => 123,
+            'test_float'  => 123.45,
+            'test_date'   => new \DateTime(),
+            'test_array'  => ['a','b','c'],
+            'test_object' => new \DateInterval('P1Y'),
+            'test_str_1'  => 'abc',
+            'test_str_2'  => '123',
+            'test_str_3'  => '123.45',
+        ]]);
+        $m = new \atk4\data\Model($p);
+        
+        $this->assertEquals('string', $m->getElement('name')->type);
+        $this->assertEquals('integer', $m->getElement('test_int')->type);
+        $this->assertEquals('float', $m->getElement('test_float')->type);
+        $this->assertEquals('datetime', $m->getElement('test_date')->type);
+        $this->assertEquals('array', $m->getElement('test_array')->type);
+        $this->assertEquals('object', $m->getElement('test_object')->type);
+        $this->assertEquals('string', $m->getElement('test_str_1')->type);
+        $this->assertEquals('string', $m->getElement('test_str_2')->type);
+        $this->assertEquals('string', $m->getElement('test_str_3')->type);
+    }
 }
 
 class StaticPersistenceModel extends \atk4\data\Model
