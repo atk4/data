atk4/data

commit eada1f06adc4c2d94ea142153b28494c19e393ff

Don't hardcode "id". Let's be smart and use owner model $id_field property instead.

src/Join.php
@@ -194,11 +194,11 @@ class Join
         } else {
             $this->reverse = false;
             if (!$this->master_field) {
-                $this->master_field = $this->foreign_table.'_id';
+                $this->master_field = $this->foreign_table.'_'.$this->owner->id_field;
             }
 
             if (!$this->foreign_field) {
-                $this->foreign_field = 'id';
+                $this->foreign_field = $this->owner->id_field;
             }
         }
 

src/Reference.php
@@ -105,7 +105,7 @@ class Reference
         if (!isset($defaults['table_alias'])) {
             if (!$this->table_alias) {
                 $this->table_alias = $this->link;
-                $this->table_alias = preg_replace('/_id/', '', $this->table_alias);
+                $this->table_alias = preg_replace('/_'.$this->owner->id_field.'/', '', $this->table_alias);
                 $this->table_alias = preg_replace('/([a-zA-Z])[a-zA-Z]*[^a-zA-Z]*/', '\1', $this->table_alias);
                 if (isset($this->owner->table_alias)) {
                     $this->table_alias = $this->owner->table_alias.'_'.$this->table_alias;

src/Reference_Many.php
@@ -54,7 +54,7 @@ class Reference_Many extends Reference
     {
         return $this->getModel($defaults)
             ->addCondition(
-                $this->their_field ?: ($this->owner->table.'_id'),
+                $this->their_field ?: ($this->owner->table.'_'.$this->owner->id_field),
                 $this->getOurValue()
             );
     }
@@ -70,7 +70,7 @@ class Reference_Many extends Reference
     {
         return $this->getModel($defaults)
             ->addCondition(
-                $this->their_field ?: ($this->owner->table.'_id'),
+                $this->their_field ?: ($this->owner->table.'_'.$this->owner->id_field),
                 $this->referenceOurValue()
             );
     }

src/Reference_SQL_One.php
@@ -147,7 +147,7 @@ class Reference_SQL_One extends Reference_One
             ]);
         }
 
-        $field = isset($defaults['field']) ? $defaults['field'] : str_replace('_id', '', $this->link);
+        $field = isset($defaults['field']) ? $defaults['field'] : str_replace('_'.$this->owner->id_field, '', $this->link);
         $ex = $this->owner->addExpression($field, array_merge_recursive(
             [
                 function ($m) {

commit e49806f9db29d9a06e011469c81e45b65408afba

add fallback to hardcoded 'id'. https://github.com/atk4/data/commit/eada1f06adc4c2d94ea142153b28494c19e393ff introduced this issue.

src/Join.php
@@ -193,12 +193,13 @@ class Join
             }
         } else {
             $this->reverse = false;
+            $id_field = $this->owner->id_field ?: 'id';
             if (!$this->master_field) {
-                $this->master_field = $this->foreign_table.'_'.$this->owner->id_field;
+                $this->master_field = $this->foreign_table.'_'.$id_field;
             }
 
             if (!$this->foreign_field) {
-                $this->foreign_field = $this->owner->id_field;
+                $this->foreign_field = $id_field;
             }
         }
 

src/Reference.php
@@ -105,7 +105,7 @@ class Reference
         if (!isset($defaults['table_alias'])) {
             if (!$this->table_alias) {
                 $this->table_alias = $this->link;
-                $this->table_alias = preg_replace('/_'.$this->owner->id_field.'/', '', $this->table_alias);
+                $this->table_alias = preg_replace('/_'.($this->owner->id_field ?: 'id').'/', '', $this->table_alias);
                 $this->table_alias = preg_replace('/([a-zA-Z])[a-zA-Z]*[^a-zA-Z]*/', '\1', $this->table_alias);
                 if (isset($this->owner->table_alias)) {
                     $this->table_alias = $this->owner->table_alias.'_'.$this->table_alias;

src/Reference_Many.php
@@ -54,7 +54,7 @@ class Reference_Many extends Reference
     {
         return $this->getModel($defaults)
             ->addCondition(
-                $this->their_field ?: ($this->owner->table.'_'.$this->owner->id_field),
+                $this->their_field ?: ($this->owner->table.'_'.($this->owner->id_field ?: 'id')),
                 $this->getOurValue()
             );
     }
@@ -70,7 +70,7 @@ class Reference_Many extends Reference
     {
         return $this->getModel($defaults)
             ->addCondition(
-                $this->their_field ?: ($this->owner->table.'_'.$this->owner->id_field),
+                $this->their_field ?: ($this->owner->table.'_'.($this->owner->id_field ?: 'id')),
                 $this->referenceOurValue()
             );
     }

src/Reference_SQL_One.php
@@ -152,7 +152,7 @@ class Reference_SQL_One extends Reference_One
 
         $field = isset($defaults['field'])
                     ? $defaults['field']
-                    : preg_replace('/_'.$this->owner->id_field.'$/i', '', $this->link);
+                    : preg_replace('/_'.($this->owner->id_field ?: 'id').'$/i', '', $this->link);
 
         $ex = $this->owner->addExpression($field, array_merge_recursive(
             [