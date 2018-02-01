832ffc38e0bd1634921ad4c6b4292f3456008d79
DarkSide <imants.horsts@inbox.lv>
2018-02-01 11:45:49 +0200

    add shortcut method for expression (reverted from commit 61920a926a723cd926c9ffc8f9977b4c81db06b9)

--- src/Model.php
+++ src/Model.php
@@ -1863,19 +1863,6 @@ class Model implements \ArrayAccess, \IteratorAggregate
         return $this->persistence->connection->lastInsertId($this);
     }
 
-    /**
-     * Creates new Expression object from expression.
-     *
-     * @param string $expr
-     * @param array  $args
-     *
-     * @return \atk4\dsql\Expression
-     */
-    public function expr($expr, $args = [])
-    {
-        return $this->persistence->expr($this, $expr, $args);
-    }
-
     // }}}
 
     // {{{ Debug Methods
