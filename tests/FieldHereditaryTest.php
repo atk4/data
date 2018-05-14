b733e4f6b06b94c2a9c0220410dc1c780875f0ec
Romans Malinovskis <me@nearly.guru>
2018-05-14 10:24:05 +0000

    Apply fixes from StyleCI

--- tests/FieldHereditaryTest.php
+++ tests/FieldHereditaryTest.php
@@ -2,9 +2,6 @@
 
 namespace atk4\data\tests;
 
-use atk4\data\Model;
-use atk4\data\Persistence_SQL;
-
 class FieldHereditaryTest extends \atk4\schema\PHPUnit_SchemaTestCase
 {
     public function testDirty1()
@@ -13,11 +10,12 @@ class FieldHereditaryTest extends \atk4\schema\PHPUnit_SchemaTestCase
 
         // default title field
         $m = new \atk4\data\Model($p);
-        $m->addField('caps', ['Callback', function($m){ return strtoupper($m['name']); }]);
+        $m->addField('caps', ['Callback', function ($m) {
+            return strtoupper($m['name']);
+        }]);
 
         $m->load(1);
         $this->assertEquals('world', $m['name']);
         $this->assertEquals('WORLD', $m['caps']);
     }
 }
-    
