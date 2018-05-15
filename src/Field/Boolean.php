9fc77fbfddafd02f4c7cbc5f8d52891972b853a9
DarkSide <imants.horsts@inbox.lv>
2018-05-15 11:01:57 +0300

    add tests (not working yet), comments, fix few bugs

--- src/Field/Boolean.php
+++ src/Field/Boolean.php
@@ -5,10 +5,11 @@
 namespace atk4\data\Field;
 
 use atk4\core\InitializerTrait;
+use atk4\data\Exception;
 use atk4\data\ValidationException;
 
 /**
- * Your favourite nullable binary type.
+ * Your favorite nullable binary type.
  */
 class Boolean extends \atk4\data\Field
 {
@@ -16,31 +17,51 @@ class Boolean extends \atk4\data\Field
         init as _init;
     }
 
-    public $valueTrue = null;
+    /**
+     * Value which will be used for "true".
+     *
+     * @var mixed
+     */
+    public $valueTrue = true;
 
-    public $valueFalse = null;
+    /**
+     * Value which will be used for "false".
+     *
+     * @var mixed
+     */
+    public $valueFalse = false;
 
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         // Backwards compatibility
-        if ($this->enum) {
+        if (isset($this->enum)) {
             $this->valueFalse = $this->enum[0];
             $this->valueTrue = $this->enum[1];
             unset($this->enum);
         }
     }
 
+    /**
+     * Normalize value to boolean value.
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
     public function normalize($value)
     {
         if (is_bool($value)) {
             return $value;
         }
 
-        if ($value === $valueTrue) {
+        if ($value === $this->valueTrue) {
             return true;
         }
 
-        if ($value === $valueFalse) {
+        if ($value === $this->valueFalse) {
             return false;
         }
 
@@ -51,10 +72,15 @@ class Boolean extends \atk4\data\Field
         throw new ValidationException([$this->name => 'Must be a boolean value']);
     }
 
-    // TODO: REVIEW
+    /**
+     * Validate if value is allowed for this field.
+     *
+     * @param mixed $value
+     */
     public function validate($value)
     {
-        if ($this->required && empty($value)) {
+        // if value required, then only valueTrue is allowed
+        if ($this->required && $value !== $this->valueTrue) {
             throw new ValidationException([$this->name => 'Must be selected']);
         }
     }
