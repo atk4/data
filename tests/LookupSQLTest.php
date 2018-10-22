5a66a1f83e15ef7b1147360fcce4d42c4c11366f
Romans Malinovskis <romans@agiletoolkit.org>
2018-10-22 14:19:17 +0100

    added tes-cases

--- tests/LookupSQLTest.php
+++ tests/LookupSQLTest.php
@@ -28,7 +28,7 @@ class LCountry extends \atk4\data\Model {
         $this->addField('name');
         $this->addField('code');
 
-        $this->addField('is_eu', ['type'=>'boolean']);
+        $this->addField('is_eu', ['type'=>'boolean', 'default'=>false]);
 
         $this->hasMany('Users', new LUser())
             ->addField('user_names', 'name');
@@ -89,6 +89,9 @@ class LFriend extends \atk4\data\Model {
     function init() {
         parent::init();
 
+        $this->addField('name');
+        $this->addField('is_vip', ['type'=>'boolean', 'default'=>false]);
+
         $this->hasOne('user_id', new User())
             ->addField('my_name', 'name');
         $this->hasOne('friend_id', new User())
@@ -133,8 +136,10 @@ class LFriend extends \atk4\data\Model {
  */
 class LookupSQLTest extends \atk4\schema\PHPUnit_SchemaTestCase
 {
-    public function testBasicLookup()
+    public function setUp()
     {
+        parent::setUp();
+
         $a = [
             'user' => [
                 1 => ['id' => 1, 'name' => 'John', 'code'=>'JN'],
@@ -151,37 +156,70 @@ class LookupSQLTest extends \atk4\schema\PHPUnit_SchemaTestCase
             ], ];
         $this->setDB($a);
 
-        //$db = new Persistence_SQL($this->db->connection);
-        $u = (new Model($this->db, 'user'))->addFields(['name']);
-        $o = (new Model($this->db, 'order'))->addFields(['amount']);
-
-        $o->hasOne('user_id')->withTitle()->addField('code');
-
-        $o->save([
-            'amount'=>11,
-            'user_id'=>2
-        ]);
-
-
-        /*
-        $o->insert([
-            'amount'=>12,
-            'user_id'=>2
-        ]);
-
-        // lookup by user title
-        $o->insert([
-            'amount'=>13,
-            'user'=>'Peter'
-        ]);
-
-        // lookup by code
-        $o->insert([
-            'amount'=>14,
-            'code'=>'JN'
-        ]);
-         */
-
-        var_Dump($this->getDB()['order']);
     }
+
+    /**
+     * test various ways to import countries
+     */
+    public function importCountriesBasic()
+    {
+        $country = new LCountry($this->db);
+
+        // should be OK, will set country name, rest of fields will be null
+        $c->save('Canada');
+
+        // adds another country
+        $c->save(['Latvia', 'code'=>'LV', 'is_eu'=>true]);
+
+        // is_eu will BLEED into this record, because save() is just set+save
+        $c->save(['Estonia', 'code'=>'ES']);
+
+        // is_eu will NOT BLEED into this record, because insert() does not make use of current model values.
+        $c->insert(['Korea', 'code'=>'KR']);
+
+        // is_eu will NOT BLEED into Japan or Russia, because import() treats all records individually
+        $c->import([
+            ['Japan', 'code'=>'JP'],
+            ['Lithuania', 'code'=>'LT', 'is_eu'=>true],
+            ['Russia', 'code'=>'KR'],
+        ]);
+
+        // Test DB contents here
+        // $this->getDB()
+    }
+
+    public function importInternationalUsers()
+    {
+        $country = new LCountry($this->db);
+
+        // Specifying hasMany here will perform input
+        $c->insert(['Canada', 'Users'=>['Alain', ['Duncan', 'is_vip'=>true]]]);
+
+        // Both lines will work quite similar
+        $c->insert(['Latvia', 'user_names'=>'imants,juris']);
+    }
+
+    public function importInternationalFriends()
+    {
+        $country = new LCountry($this->db);
+
+        // Specifying hasMany here will perform input
+        $c->insert(['Canada', 'Users'=>['Alain', ['Duncan', 'is_vip'=>true]]]);
+
+
+        // Inserting Users into Latvia can also specify Friends. In this case Friend name will be looked up
+        $c->insert(['Latvia', 'Users'=>['Imants', ['Juris', 'friend_names'=>'Alain,Imants']]]);
+
+        // Inserting This time explicitly specify friend attributes
+        $c->insert(['UK', 'Users'=>[
+            ['Romans', 'Friends'=>[
+                ['friend_id'=>1], 
+                ['friend_name'=>'Juris']
+                ['Alain']
+            ]]
+        ]]);
+
+        // BTW - Alain should have 3 friends here
+    }
+
 }
