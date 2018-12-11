d31bf3af9e3a4ea6d362ccfb222a3a89ba3e7701
DarkSide <imants.horsts@inbox.lv>
2018-12-11 23:19:17 +0200

    drop table if exists before creating

--- tests/LookupSQLTest.php
+++ tests/LookupSQLTest.php
@@ -160,9 +160,9 @@ class LookupSQLTest extends \atk4\schema\PHPUnit_SchemaTestCase
         parent::setUp();
 
         // populate database for our three models
-        $this->getMigration(new LCountry($this->db))->create();
-        $this->getMigration(new LUser($this->db))->create();
-        $this->getMigration(new LFriend($this->db))->create();
+        $this->getMigration(new LCountry($this->db))->drop()->create();
+        $this->getMigration(new LUser($this->db))->drop()->create();
+        $this->getMigration(new LFriend($this->db))->drop()->create();
     }
 
     /**
