a810f255869a117c8e7b603d334580152273be1a
Romans Malinovskis <romans@agiletoolkit.org>
2018-12-12 22:09:17 +0000

    added more complex tests and comments

--- tests/DeepCopyTest.php
+++ tests/DeepCopyTest.php
@@ -17,6 +17,7 @@ class DCClient extends Model
 
         $this->hasMany('Invoices', new DCInvoice());
         $this->hasMany('Quotes', new DCQuote());
+        $this->hasMany('Payments', new DCPayment());
     }
 
 }
@@ -163,22 +164,28 @@ class DeepCopyTest extends \atk4\schema\PHPUnit_SchemaTestCase
         $invoice = $dc
             ->from($quote)
             ->to(new DCInvoice())
-            ->with(['Lines', 'client_id'])
+            ->with(['Lines'])
             ->copy();
 
         // price now will be with VAT
         $this->assertEquals(108.90, $invoice['total']);
         $this->assertEquals(1, $invoice->id);
-        $this->assertEquals('John', $invoice->ref('client_id')['name']);
-        $this->assertNotEquals($quote['client_id'], $invoice['client_id']);
 
-        // now to add payment for the invoice
+        // Note that we did not specify that 'client_id' should be copied, so same value here
+        $this->assertEquals($quote['client_id'], $invoice['client_id']);
+        $this->assertEquals('John', $invoice->ref('client_id')['name']);
+
+        // now to add payment for the invoice. Payment originates from the same client as noted on the invoice
         $invoice->ref('Payments')->insert(['amount'=>$invoice['total'] - 5, 'client_id'=>$invoice['client_id']]);
 
         $invoice->reload();
 
+        // now that invoice is mostly paid, due amount will reflect that
         $this->assertEquals(5, $invoice['due']);
 
+        // Next we copy invocie into simply a new record. Duplicate. However this time we will also duplicate payments,
+        // and client. Because Payment references client too, we need to duplicate that one also, this way new record
+        // structure will not be related to any existing records.
         $dc = new DeepCopy();
         $invoice_copy = $dc
             ->from($invoice)
@@ -186,9 +193,64 @@ class DeepCopyTest extends \atk4\schema\PHPUnit_SchemaTestCase
             ->with(['Lines', 'client_id', 'Payments'=>['client_id']])
             ->copy();
 
-        $this->assertEquals(2, $invoice_copy->id);
+        // Invoice copy receives a new ID
+        $this->assertNotEquals($invoice->id, $invoice_copy->id);
+
+        // ..however the due amount is the same - 5
         $this->assertEquals(5, $invoice_copy['due']);
 
+        // ..client record was created in the process
+        $this->assertNotEquals($invoice_copy['client_id'], $invoice['client_id']);
+
+        // ..but he is still called John
+        $this->assertEquals('John', $invoice_copy->ref('client_id')['name']);
+
+        // finally, the client_id used for newly created payment and new invoice correspond
         $this->assertEquals($invoice_copy['client_id'], $invoice_copy->ref('Payments')->loadAny()['client_id']);
+
+
+        // the final test is to copy client entirely!
+
+
+        $dc = new DeepCopy();
+        $client3 = $dc
+            ->from((new DCClient($this->db))->load(1))
+            ->to(new DCClient())
+            ->with([
+
+                // Invoices are copied, but unless we also copy lines, totals won't be there!
+                'Invoices'=>[
+                    'Lines'
+                ],
+                'Quotes'=>[
+                    'Lines'
+                ],
+                'Payments'=>[
+
+                    // this is important to have here, because we want copied payments to be linked with NEW invoices!
+                    'invoice_id'
+                ]
+            ])
+            ->copy();
+
+        // New client receives new ID, but also will have all the relevant records copied
+        $this->assertEquals(3, $client3->id);
+
+        // We should have one of each records for this new client
+        $this->assertEquals(1, $client3->ref('Invoices')->action('count')->getOne());
+        $this->assertEquals(1, $client3->ref('Quotes')->action('count')->getOne());
+        $this->assertEquals(1, $client3->ref('Payments')->action('count')->getOne());
+
+        // We created invoice for 90 for client1, so after copying it should still be 90
+        $this->assertEquals(90, $client3->ref('Quotes')->action('fx', ['sum', 'total'])->getOne());
+
+        // The total of the invoice we copied, should remain, it's calculated based on lines
+        $this->assertEquals(108.9, $client3->ref('Invoices')->action('fx', ['sum', 'total'])->getOne());
+
+        // Payments by this clients should also be copied correctly
+        $this->assertEquals(103.9, $client3->ref('Payments')->action('fx', ['sum', 'amount'])->getOne());
+
+        // If copied payments are properly allocated against copied invoices, then due amount will be 5
+        $this->assertEquals(5, $client3->ref('Invoices')->action('fx', ['sum', 'due'])->getOne());
     }
 }
