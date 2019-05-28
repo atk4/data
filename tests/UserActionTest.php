40a214ba419e428860aca64c0b76b7f32fd5f276
Romans Malinovskis <me@nearly.guru>
2019-05-28 20:09:21 +0000

    Apply fixes from StyleCI

--- tests/UserActionTest.php
+++ tests/UserActionTest.php
@@ -98,11 +98,15 @@ class UserActionTest extends \atk4\schema\PHPUnit_SchemaTestCase
     public function testPreview()
     {
         $client = new ACClient($this->pers);
-        $client->addAction('say_a', function(){ return 'a'; });
+        $client->addAction('say_a', function () {
+            return 'a';
+        });
 
         $this->assertEquals('a', $client->getAction('say_a')->execute());
 
-        $client->getAction('say_a')->preview = function($m, $arg) { return ($m instanceof ACClient) ? 'will say '.$arg : 'will fail'; };
+        $client->getAction('say_a')->preview = function ($m, $arg) {
+            return ($m instanceof ACClient) ? 'will say '.$arg : 'will fail';
+        };
         $this->assertEquals('will say x', $client->getAction('say_a')->preview('x'));
 
         $client->addAction('also_backup', ['callback'=>'backup_clients']);
