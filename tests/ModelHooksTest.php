a39630b9465d602e4f934927d082c3f2ee636b3f
Francesco Danti <fdanti@gmail.com>
2019-09-07 17:06:01 +0300

    StyleCI fix

--- tests/ModelHooksTest.php
+++ tests/ModelHooksTest.php
@@ -19,60 +19,54 @@ class ModelHooksTest extends \atk4\schema\PHPUnit_SchemaTestCase
 
         $a = [
             'user' => [
-                1 => ['name' => 'John', 'gender' => 'M']
+                1 => ['name' => 'John', 'gender' => 'M'],
             ],
         ];
 
         $this->setDB($a);
 
-        $db      = new Persistence\SQL($this->db->connection);
+        $db = new Persistence\SQL($this->db->connection);
 
         $this->m = new Model($db, 'user');
         $this->m->addFields(['name', 'gender']);
     }
 
-    /**
-     *
-     */
     public function testBeforeSaveIsUpdate()
     {
         $this->m->addHook(
             'beforeSave', function ($m, $is_update) {
-            if ($is_update) {
-                $m->set('name', 'UpdatedModel');
-            } else {
-                $m->set('name', 'InsertedModel');
-            }
-        });
+                if ($is_update) {
+                    $m->set('name', 'UpdatedModel');
+                } else {
+                    $m->set('name', 'InsertedModel');
+                }
+            });
 
         $this->m->save();
         $this->assertEquals('InsertedModel', $this->m->get('name'));
 
         $this->m->save([
-            'gender' => 'trigger_save_with_changed_data'
+            'gender' => 'trigger_save_with_changed_data',
         ]);
         $this->assertEquals('UpdatedModel', $this->m->get('name'));
     }
 
-    /**
-     *
-     */
     public function testAfterSaveIsUpdate()
     {
         $this->m->addHook(
             'afterSave', function ($m, $is_update) {
-            if ($is_update) {
-                $m->set('name', 'UpdatedModel');
-            } else {
-                $m->set('name', 'InsertedModel');
-            }
-        });
+                if ($is_update) {
+                    $m->set('name', 'UpdatedModel');
+                } else {
+                    $m->set('name', 'InsertedModel');
+                }
+            });
 
         $this->m->save();
         $this->assertEquals('InsertedModel', $this->m->get('name'));
 
         $this->m->save([
-            'gender' => 'trigger_save_with_changed_data'
+            'gender' => 'trigger_save_with_changed_data',
         ]);
         $this->assertEquals('UpdatedModel', $this->m->get('name'));
     }
