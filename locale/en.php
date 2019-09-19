8380239eaac10380fb82f5f4f02a57f6bc45c0eb
Romans Malinovskis <me@nearly.guru>
2019-09-19 19:09:28 +0000

    Apply fixes from StyleCI

--- locale/en.php
+++ locale/en.php
@@ -1,13 +1,13 @@
 <?php
 
-return array (
-  'Unable to determine persistence driver from DSN' => 'Unable to determine persistence driver from DSN',
-  'Model is already related to another persistence' => 'Model is already related to another persistence',
-  'Mandatory field value cannot be null' => 'Mandatory field value cannot be null',
-  'Unable to typecast field value on save' => 'Unable to typecast field value on save',
-  'Unable to typecast field value on load' => 'Unable to typecast field value on load',
-  'Unable to serialize field value on save' => 'Unable to serialize field value on save',
-  'Unable to serialize field value on load' => 'Unable to serialize field value on load',
+return [
+  'Unable to determine persistence driver from DSN'                        => 'Unable to determine persistence driver from DSN',
+  'Model is already related to another persistence'                        => 'Model is already related to another persistence',
+  'Mandatory field value cannot be null'                                   => 'Mandatory field value cannot be null',
+  'Unable to typecast field value on save'                                 => 'Unable to typecast field value on save',
+  'Unable to typecast field value on load'                                 => 'Unable to typecast field value on load',
+  'Unable to serialize field value on save'                                => 'Unable to serialize field value on save',
+  'Unable to serialize field value on load'                                => 'Unable to serialize field value on load',
   'Field value can not be base64 encoded because it is not of string type' => 'Field value can not be base64 encoded because it is not of string type',
-  'There was error while decoding JSON' => 'There was error while decoding JSON',
-);
+  'There was error while decoding JSON'                                    => 'There was error while decoding JSON',
+];
