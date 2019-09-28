3367ceb6b29392f4109632f090fd788a3c800890
Francesco Danti <fdanti@gmail.com>
2019-09-28 20:59:02 +0300

    get Locale from Path

---
+++ src/Locale.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace atk4\data;
+
+class Locale
+{
+    protected function __construct()
+    {
+        throw new Exception('Class Locale is needed only for locating the default translations');
+    }
+
+    public static function getPath()
+    {
+        return __DIR__ . '/../locale/';
+    }
+}
