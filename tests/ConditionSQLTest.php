6b904e01d980a5b4001d84e8a599d9e3d79d68c4
Imants Horsts <imants.horsts@inbox.lv>
2019-11-22 15:42:23 +0000

    Apply fixes from StyleCI

--- tests/ConditionSQLTest.php
+++ tests/ConditionSQLTest.php
@@ -262,7 +262,7 @@ class ConditionSQLTest extends \atk4\schema\PHPUnit_SchemaTestCase
         $this->assertEquals('Sue', $m['name']);
         $this->assertEquals('Sue', $m['name']);
 
-        $m->addCondition([ ['date', new \DateTime('08-12-1982')] ]);
+        $m->addCondition([['date', new \DateTime('08-12-1982')]]);
         $m->loadAny();
         $this->assertEquals('Sue', $m['name']);
     }
