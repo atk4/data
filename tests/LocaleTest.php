9a9d610c8d860a5d6701b7e585eec82bc8ad6c21
Romans Malinovskis <romans@agiletoolkit.org>
2019-11-25 11:58:34 +0000

    added locale test

--- tests/LocaleTest.php
+++ tests/LocaleTest.php
@@ -4,6 +4,10 @@ namespace atk4\data\tests;
 
 use atk4\data\Exception;
 use atk4\data\Locale;
+use atk4\data\Model;
+use atk4\data\Persistence;
+use atk4\core\Translator\Translator;
+
 
 class LocaleTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,5 +21,30 @@ class LocaleTest extends \PHPUnit_Framework_TestCase
     {
         $path = implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'src', '..', 'locale']).DIRECTORY_SEPARATOR;
         $this->assertEquals($path, Locale::getPath());
+    }
+
+    public function testLocaleIntegration()
+    {
+        $a = [
+            'user' => [
+                1 => ['name' => 'John', 'surname' => 'Smith'],
+                2 => ['name' => 'Sarah', 'surname' => 'Jones'],
+            ],
+        ];
+
+        $trans = Translator::instance();
+        $trans->setDefaultLocale('ru');
+
+        try {
+            $p = new Persistence\Array_($a);
+            $m = new Model($p, 'user');
+            $m->addField('name');
+            $m->addField('surname');
+            $m->load(4);
+        } catch (Exception $e) {
+            $this->assertContains('Запись', json_decode($e->getJSON(),true)['message']);
+        }
+
+        $this->fail('Expected exception');
     }
 }
