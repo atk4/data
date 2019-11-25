f6ec70ac9edc13fe00b4632284a487ae24230a44
Romans Malinovskis <me@nearly.guru>
2019-11-25 03:12:30 +0000

    Apply fixes from StyleCI

--- src/Model.php
+++ src/Model.php
@@ -14,8 +14,8 @@ use atk4\core\FactoryTrait;
 use atk4\core\HookTrait;
 use atk4\core\InitializerTrait;
 use atk4\core\NameTrait;
-use atk4\data\Util\Helper;
 use atk4\data\UserAction\Generic;
+use atk4\data\Util\Helper;
 use atk4\dsql\Query;
 use IteratorAggregate;
 
