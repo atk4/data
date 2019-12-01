a7fbf2841479f434439e4ce5232ac50849af8eff
Georgi Hristov <ghristov@gmx.de>
2019-12-01 08:25:51 +0100

    add method to remove specified action(s) from model

--- src/Model.php
+++ src/Model.php
@@ -1104,6 +1104,23 @@ class Model implements ArrayAccess, IteratorAggregate
     {
         $this->getAction($name)->execute(...$args);
     }
+        
+    /**
+     * Remove specified action(s).
+     *
+     * @param string|array $name
+     *
+     * @throws Exception
+     * @throws \atk4\core\Exception
+     */
+    public function removeAction($name)
+    {
+        foreach ((array) $name as $action) {
+            $this->_removeFromCollection($action, 'actions');
+        }
+    	
+        return $this;
+    }
 
     // }}}
 
