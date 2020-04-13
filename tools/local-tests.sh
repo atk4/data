f8fa11a3be6b02947945b585ba6ad58961eb3827
Romans Malinovskis <romans@agiletoolkit.org>
2020-04-13 13:49:45 +0100

    cleanup

--- tools/local-tests.sh
+++ tools/local-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 cd $(dirname $0)/..
-#composer install --no-suggest --prefer-dist --optimize-autoloader
 mkdir -p build/logs
-#vendor/bin/phpunit --configuration phpunit.xml --coverage-text --exclude-group dns --stop-on-failure
+
+vendor/bin/phpunit --configuration phpunit.xml --coverage-text --exclude-group dns --stop-on-failure
 vendor/bin/phpunit --configuration phpunit-mysql.xml --exclude-group dns --stop-on-failure
 vendor/bin/phpunit --configuration phpunit-pgsql.xml --exclude-group dns --stop-on-failure
