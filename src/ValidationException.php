3faa5e7119061a53ce28607ad8eea1f73fe94d46
Michael Voříšek <mvorisek@mvorisek.cz>
2020-06-06 16:27:27 +0200

    fix bad original usage

--- src/ValidationException.php
+++ src/ValidationException.php
@@ -18,30 +18,22 @@ class ValidationException extends Exception
      *
      * @return \Exception
      */
-    public function __construct($errors, $model = null, $intent = null)
+    public function __construct(array $errors, $model = null, $intent = null)
     {
+        if (count($errors) === 0) {
+            throw new Exception('Incorrect use of ValidationException, at least one error must be given');
+        }
+
         $this->errors = $errors;
 
-        $c = is_array($errors) ? count($errors) : 0;
-        if ($c > 1) {
-            return parent::__construct('Multiple unhandled validation errors')
-                ->addMoreInfo('errors', $errors)
-                ->addMoreInfo('intent', $intent)
-                ->addMoreInfo('model', $model);
+        if (count($errors) === 1) {
+            parent::__construct(reset($errors));
+            $this->addMoreInfo('field', key($errors));
+        } else {
+            parent::__construct('Multiple unhandled validation errors');
+            $this->addMoreInfo('errors', $errors)
+                ->addMoreInfo('intent', $intent);
         }
-
-        if ($c === 1) {
-            // foreach here just to get key/value from a single member
-            foreach ($errors as $field => $error) {
-                return parent::__construct($error)
-                    ->addMoreInfo('field', $field)
-                    ->addMoreInfo('model', $model);
-            }
-        }
-
-        return parent::__construct('Incorrect use of ValidationException, argument should be an array')
-            ->addMoreInfo('errors', $errors)
-            ->addMoreInfo('intent', $intent)
-            ->addMoreInfo('model', $model);
+        $this->addMoreInfo('model', $model);
     }
 }
