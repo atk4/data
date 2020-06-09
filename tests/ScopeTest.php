56e5533ced03f2291657904aee3ca71e3762a274
Georgi Hristov <ghristov@gmx.de>
2020-06-09 09:55:20 +0200

    remove copied code comments

--- tests/ScopeTest.php
+++ tests/ScopeTest.php
@@ -7,19 +7,6 @@ use atk4\data\Model\Scope\Condition;
 use atk4\data\Model\Scope\Scope;
 use atk4\dsql\Expression;
 
-/**
- * Country.
- *
- * We will have few of them. You can lookup country by name or by code. We will also try looking up country by
- * multiple fields (e.g. code and 'is_eu') to see if those work are used as AND conditions. For instance
- * is_eu=true, code='US', should not be able to lookup the country.
- *
- * Users is a reference. You can specify it as an array containing import data for and that will be inserted
- * recursively.
- *
- * We also introduced 'user_names' field, which will concatenate all user names for said country. It can also be
- * used when importing, simply provide a comma-separated string of user names and they will be CREATED for you.
- */
 class SCountry extends Model
 {
     public $table = 'country';
@@ -40,24 +27,6 @@ class SCountry extends Model
     }
 }
 
-/**
- * User.
- *
- * User has one country and may have friends. Friend is a many-to-many relationship between users.
- *
- * When importing users, you should be able to specify country using 'country_id' or using some of the
- * lookup fields: 'country', 'country_code' or 'is_eu'.
- *
- * If ID is not specified (unlike specifying null!) we will rely on the lookup fields to try and find
- * a country. If multiple lookup fields are set, we should find a country that matches them all. If country
- * cannot be found then null should be set for country_id.
- *
- * Friends is many-to-many relationship. We have 'friend_names' field which may be similar to the one we had
- * for a country. However specifying friend_names as a comma-separated value will not create any friends.
- * Instead it will look up existing records and will create "Friend" record for all of them.
- *
- * Like before Friends can also be specified as an array.
- */
 class SUser extends Model
 {
     public $table = 'user';
@@ -78,9 +47,6 @@ class SUser extends Model
     }
 }
 
-/**
- * @coversDefaultClass \atk4\data\Model
- */
 class ScopeTest extends \atk4\schema\PhpunitTestCase
 {
     protected $user;
