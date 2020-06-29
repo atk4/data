4c02006af18c038d3fb466979a76238b30c38be0
Georgi Hristov <ghristov@gmx.de>
2020-06-29 07:19:22 +0200

    [update] include depracation warnings and deprecation period

--- src/Model/HasUserActionsTrait.php
+++ src/Model/HasUserActionsTrait.php
@@ -105,50 +105,62 @@ trait HasUserActionsTrait
     }
 
     /**
-     * @deprecated use addUserAction instead
+     * @deprecated use addUserAction instead - will be removed in dec-2020
      */
     public function addAction($name, $defaults = []): Model\UserAction
     {
+        'trigger_error'('Method Model::addAction is deprecated. Use Model::addUserAction instead', E_USER_DEPRECATED);
+
         return $this->addUserAction(...func_get_args());
     }
 
     /**
-     * @deprecated use getUserActions instead
+     * @deprecated use getUserActions instead - will be removed in dec-2020
      */
     public function getActions($scope = null): array
     {
+        'trigger_error'('Method Model::getActions is deprecated. Use Model::getUserActions instead', E_USER_DEPRECATED);
+
         return $this->getUserActions(...func_get_args());
     }
 
     /**
-     * @deprecated use hasUserAction instead
+     * @deprecated use hasUserAction instead - will be removed in dec-2020
      */
     public function hasAction($name): bool
     {
+        'trigger_error'('Method Model::hasAction is deprecated. Use Model::hasUserAction instead', E_USER_DEPRECATED);
+
         return $this->hasUserAction(...func_get_args());
     }
 
     /**
-     * @deprecated use hasUserAction instead
+     * @deprecated use getUserAction instead - will be removed in dec-2020
      */
     public function getAction($name): Model\UserAction
     {
+        'trigger_error'('Method Model::getAction is deprecated. Use Model::getUserAction instead', E_USER_DEPRECATED);
+
         return $this->getUserAction(...func_get_args());
     }
 
     /**
-     * @deprecated use executeUserAction instead
+     * @deprecated use executeUserAction instead - will be removed in dec-2020
      */
     public function executeAction($name, ...$args)
     {
+        'trigger_error'('Method Model::executeAction is deprecated. Use Model::executeUserAction instead', E_USER_DEPRECATED);
+
         $this->executeUserAction(...func_get_args());
     }
 
     /**
-     * @deprecated use removeUserAction instead
+     * @deprecated use removeUserAction instead - will be removed in dec-2020
      */
     public function removeAction($name)
     {
+        'trigger_error'('Method Model::removeAction is deprecated. Use Model::removeUserAction instead', E_USER_DEPRECATED);
+
         return $this->removeUserAction(...func_get_args());
     }
 }
