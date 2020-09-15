fe19f3433c9c20cfa1b73b979066f8e64597ee4e
DarkSide <imants.horsts@inbox.lv>
2020-09-15 16:32:25 +0300

    fix serialize

--- src/Persistence.php
+++ src/Persistence.php
@@ -337,7 +337,7 @@ class Persistence
     {
         try {
             // use $f->serialize = [encode_callback, decode_callback]
-            if (is_array($f->serialize) && isset($f->serialize[0]) && ($t = $f->typecast[0]) instanceof \Closure) {
+            if (is_array($f->serialize) && isset($f->serialize[0]) && ($t = $f->serialize[0]) instanceof \Closure) {
                 return $t($f, $value, $this);
             }
 
@@ -361,7 +361,7 @@ class Persistence
     {
         try {
             // use $f->serialize = [encode_callback, decode_callback]
-            if (is_array($f->serialize) && isset($f->serialize[1]) && ($t = $f->typecast[1]) instanceof \Closure) {
+            if (is_array($f->serialize) && isset($f->serialize[1]) && ($t = $f->serialize[1]) instanceof \Closure) {
                 return $t($f, $value, $this);
             }
 
