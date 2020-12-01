08dc6a50bdedb58268bf9a253b3e9d98ed904a82
DarkSide <imants.horsts@inbox.lv>
2020-12-01 20:48:06 +0200

    fix

--- src/Reference/ContainsOne.php
+++ src/Reference/ContainsOne.php
@@ -65,7 +65,7 @@ class ContainsOne extends Reference
                 'reference' => $this,
                 'system' => $this->system,
                 'caption' => $this->caption, // it's ref models caption, but we can use it here for field too
-                'ui' => array_replace_recursive([
+                'ui' => array_merge([
                     'visible' => false, // not visible in UI Table, Grid and Crud
                     'editable' => true, // but should be editable in UI Form
                 ], $this->ui),
