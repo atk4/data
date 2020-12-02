a03dbc5c58d05545811ee8b794e428f759e04e48
Georgi Hristov <ghristov@gmx.de>
2020-12-02 10:48:06 +0100

    [update] add return types

--- src/Model/AggregatesTrait.php
+++ src/Model/AggregatesTrait.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace atk4\data\Model;
 
+use atk4\data\Model;
+
 /**
  * Provides aggregation methods.
  */
@@ -11,20 +13,16 @@ trait AggregatesTrait
 {
     /**
      * @see Aggregate::withAggregateField.
-     *
-     * @return \atk4\data\Model
      */
-    public function withAggregateField($name, $seed = [])
+    public function withAggregateField($name, $seed = []): Model
     {
         return (new Aggregate($this))->withAggregateField(...func_get_args());
     }
 
     /**
      * @see Aggregate::groupBy.
-     *
-     * @return \atk4\data\Model
      */
-    public function groupBy(array $group, array $aggregate = [])
+    public function groupBy(array $group, array $aggregate = []): Model
     {
         return (new Aggregate($this))->groupBy(...func_get_args());
     }
