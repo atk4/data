8edc810b2cdaeb090952c8e1007956be5ab67371
Michael Voříšek <mvorisek@mvorisek.cz>
2020-12-11 15:24:05 +0100

    Implicit ID have default type of integer

--- src/Model.php
+++ src/Model.php
@@ -372,7 +372,7 @@ class Model extends Model\Base implements \IteratorAggregate
         $this->_init();
 
         if ($this->id_field) {
-            $this->addField($this->id_field, ['system' => true]);
+            $this->addField($this->id_field, ['type' => 'integer', 'system' => true]);
         } else {
             return; // don't declare actions for model without id_field
         }
