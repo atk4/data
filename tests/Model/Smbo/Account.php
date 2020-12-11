0495a6ab61dbd6443c409c9e577ce65f77278d28
Michael Voříšek <mvorisek@mvorisek.cz>
2020-12-11 15:18:48 +0100

    Use short Model name everywhere

--- tests/Model/Smbo/Account.php
+++ tests/Model/Smbo/Account.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Atk4\Data\Tests\Model\Smbo;
 
-class Account extends \Atk4\Data\Model
+use Atk4\Data\Model;
+
+class Account extends Model
 {
     public $table = 'account';
 
