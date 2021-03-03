e9d43bd3ed7a4e3de8d271f7308d8232ae4058cd
Michael Voříšek <mvorisek@mvorisek.cz>
2021-03-03 23:57:25 +0100

    more robust code

--- src/Reference.php
+++ src/Reference.php
@@ -195,7 +195,7 @@ class Reference
             $ourModel = $this->getOurModel();
 
             $aliasFull = $this->link;
-            $alias = preg_replace('~_(' . preg_quote($ourModel->id_field, '~') . '|id)$~', '', $this->link);
+            $alias = preg_replace('~_(' . preg_quote($ourModel->id_field, '~') . '|id)$~', '', $aliasFull);
             $alias = preg_replace('~([0-9a-z]?)[0-9a-z]*[^0-9a-z]*~i', '$1', $alias);
             if (isset($ourModel->table_alias)) {
                 $aliasFull = $ourModel->table_alias . '_' . $aliasFull;
