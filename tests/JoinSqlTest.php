f6d7037b48ff86db3c935cb87309e8bc7be24c7d
Michael Voříšek <mvorisek@mvorisek.cz>
2021-04-28 08:52:49 +0200

    fix JoinSqlTest

--- tests/JoinSqlTest.php
+++ tests/JoinSqlTest.php
@@ -553,45 +553,45 @@ class JoinSqlTest extends \Atk4\Schema\PhpunitTestCase
         $m_u->addField('name');
         $j = $m_u->join('contact');
 
-        $m_u = $m_u->load(1);
+        $m_u2 = $m_u->load(1);
         $this->assertEquals([
             'id' => 1, 'name' => 'John', 'contact_id' => 10,
-        ], $m_u->get());
+        ], $m_u2->get());
 
         // hasOne phone model
         $m_p = new Model($db, ['table' => 'phone']);
         $m_p->addField('number');
-        $ref = $j->hasOne('phone_id', ['model' => $m_p]); // hasOne on JOIN
-        $ref->addField('number');
+        $ref_one = $j->hasOne('phone_id', ['model' => $m_p]); // hasOne on JOIN
+        $ref_one->addField('number');
 
-        $m_u = $m_u->load(1);
+        $m_u2 = $m_u->load(1);
         $this->assertEquals([
             'id' => 1, 'name' => 'John', 'contact_id' => 10, 'phone_id' => 20, 'number' => '+123',
-        ], $m_u->get());
+        ], $m_u2->get());
 
         // hasMany token model (uses default our_field, their_field)
         $m_t = new Model($db, ['table' => 'token']);
         $m_t->addField('user_id');
         $m_t->addField('token');
-        $ref = $j->hasMany('Token', ['model' => $m_t]); // hasMany on JOIN (use default our_field, their_field)
+        $ref_many = $j->hasMany('Token', ['model' => $m_t]); // hasMany on JOIN (use default our_field, their_field)
 
-        $m_u = $m_u->load(1);
+        $m_u2 = $m_u->load(1);
         $this->assertEquals([
             ['id' => 30, 'user_id' => 1, 'token' => 'ABC'],
             ['id' => 31, 'user_id' => 1, 'token' => 'DEF'],
-        ], $m_u->ref('Token')->export());
+        ], $m_u2->ref('Token')->export());
 
         // hasMany email model (uses custom our_field, their_field)
         $m_e = new Model($db, ['table' => 'email']);
         $m_e->addField('contact_id');
         $m_e->addField('address');
-        $ref = $j->hasMany('Email', ['model' => $m_e, 'our_field' => 'contact_id', 'their_field' => 'contact_id']); // hasMany on JOIN (use custom our_field, their_field)
+        $ref_many = $j->hasMany('Email', ['model' => $m_e, 'our_field' => 'contact_id', 'their_field' => 'contact_id']); // hasMany on JOIN (use custom our_field, their_field)
 
-        $m_u = $m_u->load(1);
+        $m_u2 = $m_u->load(1);
         $this->assertEquals([
             ['id' => 40, 'contact_id' => 10, 'address' => 'john@foo.net'],
             ['id' => 41, 'contact_id' => 10, 'address' => 'johnny@foo.net'],
-        ], $m_u->ref('Email')->export());
+        ], $m_u2->ref('Email')->export());
     }
 
     public function testJoinReverseOneOnOne()
