8f6352cd8991e4e2210fd206888d3ba954aa1992
Michael Voříšek <mvorisek@mvorisek.cz>
2021-11-26 18:44:58 +0100

    Add WITH support for MSSQL

--- src/Persistence/Sql/Oracle/Query.php
+++ src/Persistence/Sql/Oracle/Query.php
@@ -104,7 +104,6 @@ class Query extends BaseQuery
 
     public function limit($cnt, $shift = null)
     {
-        // This is for pre- 12c version
         $this->template_select = $this->template_select_limit;
 
         return parent::limit($cnt, $shift);
