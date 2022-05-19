54f7276cebf938fb0da7d9d62a5979e66c594f67
samsouthardjr <samsouthardjr@gmail.com>
2022-05-18 22:25:25 -0700

    Revert "ModelCheckPointTest no longer viable when checkpoints are no longer in the model"
    This reverts commit 88224307f239cc4698f8a4cc9f523578e8d61ec6.

---
+++ tests/ModelCheckpointTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Atk4\Data\Tests;
+
+use Atk4\Data\Model;
+use Atk4\Data\Schema\TestCase;
+
+class Model_Test extends Model
+{
+    public $table = 'test';
+
+    protected function init(): void
+    {
+        parent::init();
+        $this->addField('static');
+        $this->addField('changing');
+    }
+}
+
+class ModelCheckpointTest extends TestCase
+{
+    public function testCheckpoint(): void
+    {
+        $this->setDb([
+            'test' => [
+                ['id' => 1, 'static' => 'unchanging', 'changing' => 'basevalue'],
+            ],
+        ]);
+
+        $m = new Model_Test($this->db);
+
+        $m = $m->load(1);
+        $m->set('changing', 'newvalue');
+        $m->checkpoint(1);
+        $this->assertEquals([], $m->getDirtyVsCheckpoint(1));
+        $this->assertEquals(['changing' => 'basevalue'], $m->getDirtyVsCheckpoint());
+        $m->set('changing', 'basevalue');
+        $this->assertEquals([], $m->getDirtyVsCheckpoint());
+        $this->assertEquals(['changing' => 'newvalue'], $m->getDirtyVsCheckpoint(1));
+    }
+}
