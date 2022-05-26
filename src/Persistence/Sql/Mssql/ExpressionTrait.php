2ab7f65d37dadb4409c0b2873c1978f063054902
Michael Voříšek <mvorisek@mvorisek.cz>
2022-05-27 00:55:01 +0200

    Fix exception throwing for MSSQL TRY/CATCH SQL

--- src/Persistence/Sql/Mssql/ExpressionTrait.php
+++ src/Persistence/Sql/Mssql/ExpressionTrait.php
@@ -37,4 +37,41 @@ trait ExpressionTrait
     {
         return false;
     }
+
+    /**
+     * Fix exception throwing for MSSQL TRY/CATCH SQL (for Query::$template_insert).
+     *
+     * Remove once https://github.com/microsoft/msphpsql/issues/1387 is fixed and released.
+     */
+    public function execute(object $connection = null): \Doctrine\DBAL\Result
+    {
+        $templateStr = $this->template ?? 'select...'; // @phpstan-ignore-line
+        if (preg_match('~^\s*begin\s+try(.+?)end\s+try\s+begin\s+catch(.+)end\s+catch\s*$~is', $templateStr, $matches)) {
+            $templateTry = trim($matches[1]);
+            if (preg_match('~^\s+if ERROR_NUMBER\(\)\s*=\s*544\s+begin(.+)end\s+else\s+begin\s+throw;\s*end\s+$~is', $matches[2], $matches)) {
+                $templateCatch = trim($matches[1]);
+            } else {
+                throw new \Error('Unexpected MSSQL TRY/CATCH SQL');
+            }
+
+            try {
+                $thisCloned = clone $this;
+                $thisCloned->template = $templateTry;
+
+                return $thisCloned->execute($connection);
+            } catch (\Exception $e) {
+                $eDbal = $e->getPrevious();
+                if ($eDbal !== null && $eDbal instanceof \Doctrine\DBAL\Exception\DriverException && $eDbal->getCode() === 544) {
+                    $thisCloned = clone $this;
+                    $thisCloned->template = $templateCatch;
+
+                    return $thisCloned->execute($connection);
+                }
+
+                throw $e;
+            }
+        }
+
+        return parent::execute($connection);
+    }
 }
