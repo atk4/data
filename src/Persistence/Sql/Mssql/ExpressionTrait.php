26832e7e1111370cf289bb8ad389054d3f33b531
Michael Voříšek <mvorisek@mvorisek.cz>
2022-05-27 11:49:06 +0200

    Fix exception throwing for MSSQL insert queries (#993)

--- src/Persistence/Sql/Mssql/ExpressionTrait.php
+++ src/Persistence/Sql/Mssql/ExpressionTrait.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Atk4\Data\Persistence\Sql\Mssql;
 
+use Doctrine\DBAL\Exception\DriverException;
+use Doctrine\DBAL\Result as DbalResult;
+
 trait ExpressionTrait
 {
     private function fixOpenEscapeChar(string $v): string
@@ -36,5 +39,74 @@ trait ExpressionTrait
     protected function hasNativeNamedParamSupport(): bool
     {
         return false;
+    }
+
+    /**
+     * Fix exception throwing for MSSQL TRY/CATCH SQL (for Query::$template_insert).
+     *
+     * Remove once https://github.com/microsoft/msphpsql/issues/1387 is fixed and released.
+     */
+    public function execute(object $connection = null): DbalResult
+    {
+        $templateStr = preg_replace('~^\s*begin\s+(.+?)\s+end\s*$~is', '$1', $this->template ?? 'select...'); // @phpstan-ignore-line
+        if (preg_match('~^(.*?)begin\s+try(.+?)end\s+try\s+begin\s+catch(.+)end\s+catch(.*?)$~is', $templateStr, $matches)) {
+            $executeFx = function (string $template) use ($connection): DbalResult {
+                $thisCloned = clone $this;
+                $thisCloned->template = !str_contains(trim(trim($template), ';'), ';')
+                    ? $template
+                    : 'BEGIN' . "\n" . $template . "\n" . 'END';
+
+                return $thisCloned->execute($connection);
+            };
+
+            $templateBefore = trim($matches[1]);
+            $templateTry = trim($matches[2]);
+            $templateAfter = trim($matches[4]);
+
+            $expectedInsertTemplate = <<<'EOF'
+                begin try
+                  insert[option] into [table_noalias] ([set_fields]) values ([set_values]);
+                end try begin catch
+                  if ERROR_NUMBER() = 544 begin
+                    set IDENTITY_INSERT [table_noalias] on;
+                    begin try
+                      insert[option] into [table_noalias] ([set_fields]) values ([set_values]);
+                      set IDENTITY_INSERT [table_noalias] off;
+                    end try begin catch
+                      set IDENTITY_INSERT [table_noalias] off;
+                      throw;
+                    end catch
+                  end else begin
+                    throw;
+                  end
+                end catch
+                EOF;
+
+            if ($templateBefore === '' && $templateAfter === '' && $templateStr === $expectedInsertTemplate) {
+                $executeCatchFx = function (\Exception $e) use ($executeFx): DbalResult {
+                    $eDriver = $e->getPrevious();
+                    if ($eDriver !== null && $eDriver instanceof DriverException && $eDriver->getCode() === 544) {
+                        try {
+                            return $executeFx('set IDENTITY_INSERT [table_noalias] on;'
+                                . "\n" . 'insert[option] into [table_noalias] ([set_fields]) values ([set_values]);');
+                        } finally {
+                            $executeFx('set IDENTITY_INSERT [table_noalias] off;');
+                        }
+                    }
+
+                    throw $e;
+                };
+            } else {
+                throw new \Error('Unexpected MSSQL TRY/CATCH SQL: ' . $templateStr);
+            }
+
+            try {
+                return $executeFx($templateTry);
+            } catch (\Exception $e) {
+                return $executeCatchFx($e);
+            }
+        }
+
+        return parent::execute($connection);
     }
 }
