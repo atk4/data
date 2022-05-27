1ef57e6ae53d8641772f0d5ab1d669614db0059d
Michael Voříšek <mvorisek@mvorisek.cz>
2022-05-27 02:51:01 +0200

    also for inner

--- src/Persistence/Sql/Mssql/ExpressionTrait.php
+++ src/Persistence/Sql/Mssql/ExpressionTrait.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Atk4\Data\Persistence\Sql\Mssql;
 
+use Doctrine\DBAL\Exception\DriverException;
+use Doctrine\DBAL\Result as DbalResult;
+
 trait ExpressionTrait
 {
     private function fixOpenEscapeChar(string $v): string
@@ -43,32 +46,49 @@ trait ExpressionTrait
      *
      * Remove once https://github.com/microsoft/msphpsql/issues/1387 is fixed and released.
      */
-    public function execute(object $connection = null): \Doctrine\DBAL\Result
+    public function execute(object $connection = null): DbalResult
     {
-        $templateStr = $this->template ?? 'select...'; // @phpstan-ignore-line
-        if (preg_match('~^\s*begin\s+try(.+?)end\s+try\s+begin\s+catch(.+)end\s+catch\s*$~is', $templateStr, $matches)) {
-            $templateTry = trim($matches[1]);
-            if (preg_match('~^\s+if ERROR_NUMBER\(\)\s*=\s*544\s+begin(.+)end\s+else\s+begin\s+throw;\s*end\s+$~is', $matches[2], $matches)) {
-                $templateCatch = trim($matches[1]);
+        $templateStr = preg_replace('~^\s*begin\s+(.+)\s+end\s*$~is', '$1', $this->template ?? 'select...'); // @phpstan-ignore-line
+        if (preg_match('~^(.*?)begin\s+try(.+?)end\s+try\s+begin\s+catch(.+)end\s+catch(.*?)$~is', $templateStr, $matches)) {
+            $executeFx = function (string $template) use ($connection): DbalResult {
+                $thisCloned = clone $this;
+                $thisCloned->template = !str_contains(trim(trim($template), ';'), ';')
+                    ? $template
+                    : 'BEGIN' . "\n" . $template . "\n" . 'END';
+
+                return $thisCloned->execute($connection);
+            };
+
+            $templateBefore = trim($matches[1]);
+            $templateTry = trim($matches[2]);
+            $templateAfter = trim($matches[4]);
+
+            if ($templateBefore === '' && $templateAfter === '' && preg_match('~^\s+if ERROR_NUMBER\(\)\s*=\s*544\s+begin\s*(.+?)\s*end\s+else\s+begin\s+throw;\s*end\s+$~is', $matches[3], $matches2)) {
+                $templateCatch = 'set IDENTITY_INSERT [table_noalias] on;'
+                    . "\n" . 'insert[option] into [table_noalias] ([set_fields]) values ([set_values]);';
+                $templateCatchFinally = 'set IDENTITY_INSERT [table_noalias] off;';
+                $executeCatchFx = function (\Exception $e) use ($executeFx, $templateCatch, $templateCatchFinally): DbalResult {
+                    $eDriver = $e->getPrevious();
+                    if ($eDriver !== null && $eDriver instanceof DriverException && $eDriver->getCode() === 544) {
+                        try {
+                            return $executeFx($templateCatch);
+                        } finally {
+                            $executeFx($templateCatchFinally);
+                        }
+                    }
+
+                    throw $e;
+                };
             } else {
-                throw new \Error('Unexpected MSSQL TRY/CATCH SQL');
+                throw new \Error('Unexpected MSSQL TRY/CATCH SQL: ' . $templateStr);
             }
 
             try {
-                $thisCloned = clone $this;
-                $thisCloned->template = $templateTry;
+                $res = $executeFx($templateTry);
 
-                return $thisCloned->execute($connection);
+                return $res;
             } catch (\Exception $e) {
-                $eDbal = $e->getPrevious();
-                if ($eDbal !== null && $eDbal instanceof \Doctrine\DBAL\Exception\DriverException && $eDbal->getCode() === 544) {
-                    $thisCloned = clone $this;
-                    $thisCloned->template = $templateCatch;
-
-                    return $thisCloned->execute($connection);
-                }
-
-                throw $e;
+                return $executeCatchFx($e);
             }
         }
 
