c674bfd1c2be00c6eb3ce5a38d5952d4d0b83ea3
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-06 22:46:13 +0200

    SMBO Contact/Company test models are unused

--- tests/Model/Smbo/Document.php
+++ tests/Model/Smbo/Document.php
@@ -14,10 +14,6 @@ class Document extends Model
     {
         parent::init();
 
-        // Documest is sent from one Contact to Another
-        $this->hasOne('contact_from_id', ['model' => [Contact::class]]);
-        $this->hasOne('contact_to_id', ['model' => [Contact::class]]);
-
         $this->addField('doc_type', ['enum' => ['invoice', 'payment']]);
 
         $this->addField('amount', ['type' => 'atk4_money']);
