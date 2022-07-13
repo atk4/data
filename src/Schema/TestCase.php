b770924adad35d8060898241bc8ea1d6cc0a27e2
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-14 01:02:09 +0200

    fix cs

--- src/Schema/TestCase.php
+++ src/Schema/TestCase.php
@@ -63,22 +63,22 @@ abstract class TestCase extends BaseTestCase
 
                     echo "\n" . $sql . (substr($sql, -1) !== ';' ? ';' : '') . "\n"
                         . (is_array($params) && count($params) > 0 ? substr(print_r(array_map(function ($v) {
-                        if ($v === null) {
-                            $v = 'null';
-                        } elseif (is_bool($v)) {
-                            $v = $v ? 'true' : 'false';
-                        } elseif (is_float($v) && (string) $v === (string) (int) $v) {
-                            $v = $v . '.0';
-                        } elseif (is_string($v)) {
-                            if (strlen($v) > 4096) {
-                                $v = '*long string* (length: ' . strlen($v) . ' bytes, sha256: ' . hash('sha256', $v) . ')';
-                            } else {
-                                $v = '\'' . $v . '\'';
+                            if ($v === null) {
+                                $v = 'null';
+                            } elseif (is_bool($v)) {
+                                $v = $v ? 'true' : 'false';
+                            } elseif (is_float($v) && (string) $v === (string) (int) $v) {
+                                $v = $v . '.0';
+                            } elseif (is_string($v)) {
+                                if (strlen($v) > 4096) {
+                                    $v = '*long string* (length: ' . strlen($v) . ' bytes, sha256: ' . hash('sha256', $v) . ')';
+                                } else {
+                                    $v = '\'' . $v . '\'';
+                                }
                             }
-                        }
 
-                        return $v;
-                    }, $params), true), 6) : '') . "\n";
+                            return $v;
+                        }, $params), true), 6) : '') . "\n";
                 }
 
                 public function stopQuery(): void
