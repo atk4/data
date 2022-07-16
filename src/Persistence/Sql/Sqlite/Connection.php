7d0ea740ad50b19c4f47694387b5492dea9a9ee4
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-16 18:53:59 +0200

    Connect to test DB lazily from TestCase

--- src/Persistence/Sql/Sqlite/Connection.php
+++ src/Persistence/Sql/Sqlite/Connection.php
@@ -5,8 +5,32 @@ declare(strict_types=1);
 namespace Atk4\Data\Persistence\Sql\Sqlite;
 
 use Atk4\Data\Persistence\Sql\Connection as BaseConnection;
+use Doctrine\Common\EventManager;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Event\ConnectionEventArgs;
+use Doctrine\DBAL\Events;
 
 class Connection extends BaseConnection
 {
     protected $queryClass = Query::class;
+
+    protected static function createDbalEventManager(): EventManager
+    {
+        $evm = parent::createDbalEventManager();
+
+        // setup connection to always check foreign keys
+        $evm->addEventSubscriber(new class() implements EventSubscriber {
+            public function getSubscribedEvents(): array
+            {
+                return [Events::postConnect];
+            }
+
+            public function postConnect(ConnectionEventArgs $args): void
+            {
+                $args->getConnection()->executeStatement('PRAGMA foreign_keys = 1');
+            }
+        });
+
+        return $evm;
+    }
 }
--- src/Schema/TestCase.php
+++ src/Schema/TestCase.php
@@ -25,64 +25,86 @@ abstract class TestCase extends BaseTestCase
     /** @var Migrator[] */
     private $createdMigrators = [];
 
+    /**
+     * @return static|null
+     */
+    public static function getTestFromBacktrace()
+    {
+        foreach (debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS | \DEBUG_BACKTRACE_PROVIDE_OBJECT) as $frame) {
+            if (($frame['object'] ?? null) instanceof static) {
+                return $frame['object']; // @phpstan-ignore-line https://github.com/phpstan/phpstan/issues/7639
+            }
+        }
+
+        return null;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->db = Persistence::connect($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        // create SQL persistence with lazy connect
+        $this->db = new class() extends Persistence\Sql {
+            public function __construct()
+            {
+            }
 
-        if ($this->getDatabasePlatform() instanceof SqlitePlatform) {
-            $this->getConnection()->expr(
-                'PRAGMA foreign_keys = 1'
-            )->executeStatement();
-        }
-        if ($this->getDatabasePlatform() instanceof MySQLPlatform) {
-            $this->getConnection()->expr(
-                'SET SESSION auto_increment_increment = 1, SESSION auto_increment_offset = 1'
-            )->executeStatement();
-        }
+            public function getConnection(): Persistence\Sql\Connection
+            {
+                \Closure::bind(function () {
+                    if ($this->_connection === null) {
+                        $connection = Persistence\Sql::assertInstanceOf(
+                            Persistence::connect($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'])
+                        )->_connection;
+                        $this->_connection = $connection;
 
-        $this->getConnection()->getConnection()->getConfiguration()->setSQLLogger(
-            null ?? new class($this) implements SQLLogger { // @phpstan-ignore-line
-                /** @var \WeakReference<TestCase> */
-                private $testCaseWeakRef;
+                        if ($connection->getDatabasePlatform() instanceof MySQLPlatform) {
+                            $connection->expr(
+                                'SET SESSION auto_increment_increment = 1, SESSION auto_increment_offset = 1'
+                            )->executeStatement();
+                        }
 
-                public function __construct(TestCase $testCase)
-                {
-                    $this->testCaseWeakRef = \WeakReference::create($testCase);
-                }
+                        $connection->getConnection()->getConfiguration()->setSQLLogger(
+                            // @phpstan-ignore-next-line SQLLogger is deprecated
+                            null ?? new class() implements SQLLogger {
+                                public function startQuery($sql, array $params = null, array $types = null): void
+                                {
+                                    $test = TestCase::getTestFromBacktrace();
+                                    if (!$test->debug) {
+                                        return;
+                                    }
 
-                public function startQuery($sql, array $params = null, array $types = null): void
-                {
-                    if (!$this->testCaseWeakRef->get()->debug) {
-                        return;
-                    }
+                                    echo "\n" . $sql . (substr($sql, -1) !== ';' ? ';' : '') . "\n"
+                                        . (is_array($params) && count($params) > 0 ? substr(print_r(array_map(function ($v) {
+                                            if ($v === null) {
+                                                $v = 'null';
+                                            } elseif (is_bool($v)) {
+                                                $v = $v ? 'true' : 'false';
+                                            } elseif (is_float($v) && (string) $v === (string) (int) $v) {
+                                                $v = $v . '.0';
+                                            } elseif (is_string($v)) {
+                                                if (strlen($v) > 4096) {
+                                                    $v = '*long string* (length: ' . strlen($v) . ' bytes, sha256: ' . hash('sha256', $v) . ')';
+                                                } else {
+                                                    $v = '\'' . $v . '\'';
+                                                }
+                                            }
 
-                    echo "\n" . $sql . (substr($sql, -1) !== ';' ? ';' : '') . "\n"
-                        . (is_array($params) && count($params) > 0 ? substr(print_r(array_map(function ($v) {
-                            if ($v === null) {
-                                $v = 'null';
-                            } elseif (is_bool($v)) {
-                                $v = $v ? 'true' : 'false';
-                            } elseif (is_float($v) && (string) $v === (string) (int) $v) {
-                                $v = $v . '.0';
-                            } elseif (is_string($v)) {
-                                if (strlen($v) > 4096) {
-                                    $v = '*long string* (length: ' . strlen($v) . ' bytes, sha256: ' . hash('sha256', $v) . ')';
-                                } else {
-                                    $v = '\'' . $v . '\'';
+                                            return $v;
+                                        }, $params), true), 6) : '') . "\n";
+                                }
+
+                                public function stopQuery(): void
+                                {
                                 }
                             }
+                        );
+                    }
+                }, $this, Persistence\Sql::class)();
 
-                            return $v;
-                        }, $params), true), 6) : '') . "\n";
-                }
-
-                public function stopQuery(): void
-                {
-                }
+                return parent::getConnection();
             }
-        );
+        };
     }
 
     protected function tearDown(): void
