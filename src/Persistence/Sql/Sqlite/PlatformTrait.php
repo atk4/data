32cee4d4157bffd581964df408950129464ce801
Michael Voříšek <mvorisek@mvorisek.cz>
2022-07-19 21:06:06 +0200

    backport DBAL 5517 - TODO needed priod DBAL 3.4, remove it !!!

--- src/Persistence/Sql/Sqlite/PlatformTrait.php
+++ src/Persistence/Sql/Sqlite/PlatformTrait.php
@@ -9,6 +9,13 @@ use Doctrine\DBAL\Schema\TableDiff;
 
 trait PlatformTrait
 {
+    use PlatformTraitBackport5517 {
+        PlatformTraitBackport5517::getListTableConstraintsSQL as private __getListTableConstraintsSQL;
+        PlatformTraitBackport5517::getListTableColumnsSQL as private __getListTableColumnsSQL;
+        PlatformTraitBackport5517::getListTableIndexesSQL as private __getListTableIndexesSQL;
+        PlatformTraitBackport5517::getListTableForeignKeysSQL as private __getListTableForeignKeysSQL;
+    }
+
     public function __construct()
     {
         $this->disableSchemaEmulation(); // @phpstan-ignore-line
@@ -18,6 +25,46 @@ trait PlatformTrait
     public function getIdentifierQuoteCharacter(): string
     {
         return '`';
+    }
+
+    public function supportsForeignKeyConstraints(): bool
+    {
+        // backport https://github.com/doctrine/dbal/pull/5427, remove once DBAL 3.3.x support is dropped
+        return true;
+    }
+
+    protected function getPreAlterTableIndexForeignKeySQL(TableDiff $diff): array
+    {
+        // https://github.com/doctrine/dbal/pull/5486
+        return [];
+    }
+
+    // fix quoted table name support
+    // TODO submit a PR with fixed SQLitePlatform to DBAL
+
+    private function unquoteTableIdentifier(string $tableName): string
+    {
+        return (new Identifier($tableName))->getName();
+    }
+
+    public function getListTableConstraintsSQL($table)
+    {
+        return $this->__getListTableConstraintsSQL($this->unquoteTableIdentifier($table));
+    }
+
+    public function getListTableColumnsSQL($table, $database = null)
+    {
+        return $this->__getListTableColumnsSQL($this->unquoteTableIdentifier($table), $database);
+    }
+
+    public function getListTableIndexesSQL($table, $database = null)
+    {
+        return $this->__getListTableIndexesSQL($this->unquoteTableIdentifier($table), $database);
+    }
+
+    public function getListTableForeignKeysSQL($table, $database = null)
+    {
+        return $this->__getListTableForeignKeysSQL($this->unquoteTableIdentifier($table), $database);
     }
 
     #[\Override]
