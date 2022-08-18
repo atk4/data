5352caff2868892d630a7bbf2690d3cc1f9aa1d8
Michael Voříšek <mvorisek@mvorisek.cz>
2022-08-18 16:29:05 +0200

    Revert "trait for easy setup"
    This reverts commit 996813103c2d397c65c07f6f98faaf17e3c18164.

--- tests/RandomTest.php
+++ tests/RandomTest.php
@@ -27,8 +27,6 @@ class Model_Rate extends Model
 }
 class Model_Item extends Model
 {
-    use ModelSoftDeleteTrait;
-
     public $table = 'item';
 
     protected function init(): void
@@ -39,8 +37,15 @@ class Model_Item extends Model
         $this->hasOne('parent_item_id', ['model' => [self::class]])
             ->addTitle();
 
-        $this->initSoftDelete();
+        $this->addField('is_deleted', ['type' => 'boolean', 'nullable' => false, 'default' => false]);
+        $this->addCondition('is_deleted', false);
+        $this->onHook(Model::HOOK_BEFORE_DELETE, function (Model $entity) {
+            $softDeleteController = new ControllerSoftDelete();
+            $softDeleteController->softDelete($entity);
 
+            $entity->hook(Model::HOOK_AFTER_DELETE);
+            $entity->breakHook(false); // this will cancel original Model::delete()
+        });
     }
 }
 class Model_Item2 extends Model
@@ -73,21 +78,6 @@ class Model_Item3 extends Model
 
         $this->hasMany('Child', ['model' => [self::class], 'theirField' => 'parent_item_id', 'tableAlias' => 'child'])
             ->addField('child_age', ['aggregate' => 'sum', 'field' => 'age']);
-    }
-}
-
-trait ModelSoftDeleteTrait {
-    protected function initSoftDelete(): void
-    {
-        $this->addField('is_deleted', ['type' => 'boolean', 'nullable' => false, 'default' => false]);
-        $this->addCondition('is_deleted', false);
-        $this->onHook(Model::HOOK_BEFORE_DELETE, function (Model $entity) {
-            $softDeleteController = new ControllerSoftDelete();
-            $softDeleteController->softDelete($entity);
-
-            $entity->hook(Model::HOOK_AFTER_DELETE);
-            $entity->breakHook(false); // this will cancel original Model::delete()
-        });
     }
 }
 
