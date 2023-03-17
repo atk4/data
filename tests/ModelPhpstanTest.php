2de50a021cf13070269c3872433cce8102eb62e0
Michael Voříšek <mvorisek@mvorisek.cz>
2023-03-17 20:48:17 +0100

    fix dynamic call of static method target

--- tests/ModelPhpstanTest.php
+++ tests/ModelPhpstanTest.php
@@ -20,58 +20,58 @@ class ModelPhpstanTest extends TestCase
     public function testVirtualInterfacesSimple(): void
     {
         $model = new Model();
-        $this->assertSamePhpstanType(Model::class, $model);
-        $this->assertSamePhpstanType(Model::class, get_class($model)::assertInstanceOf($model));
+        static::assertSamePhpstanType(Model::class, $model);
+        static::assertSamePhpstanType(Model::class, get_class($model)::assertInstanceOf($model));
 
         $entity = $model->createEntity();
-        $this->assertSamePhpstanType(Model::class . '&' . IsEntity::class, $entity);
-        $this->assertSamePhpstanType(Model::class, $entity->getModel());
+        static::assertSamePhpstanType(Model::class . '&' . IsEntity::class, $entity);
+        static::assertSamePhpstanType(Model::class, $entity->getModel());
 
         if (PhpstanUtil::alwaysFalseAnalyseOnly()) {
             $model->assertIsEntity();
-            $this->assertSamePhpstanType(Model::class . '&' . IsEntity::class, $model);
+            static::assertSamePhpstanType(Model::class . '&' . IsEntity::class, $model);
             $model->assertIsModel();
-            $this->assertSamePhpstanType(Model::class, $model);
+            static::assertSamePhpstanType(Model::class, $model);
 
             $entity = $model->load(1);
-            $this->assertSamePhpstanType(Model::class . '&' . IsLoaded::class, $entity);
+            static::assertSamePhpstanType(Model::class . '&' . IsLoaded::class, $entity);
             $entity->unload();
-            $this->assertSamePhpstanType(Model::class . '&' . IsEntity::class, $entity);
+            static::assertSamePhpstanType(Model::class . '&' . IsEntity::class, $entity);
             $entity->assertIsLoaded();
-            $this->assertSamePhpstanType(Model::class . '&' . IsLoaded::class, $entity);
+            static::assertSamePhpstanType(Model::class . '&' . IsLoaded::class, $entity);
             $entity->assertIsEntity();
-            $this->assertSamePhpstanType(Model::class . '&' . IsLoaded::class, $entity);
+            static::assertSamePhpstanType(Model::class . '&' . IsLoaded::class, $entity);
         }
 
         // model and entity is mutually exclusive
-        $this->assertSamePhpstanType(Model::class, random_int(0, 1) === 0 ? $model : $entity);
+        static::assertSamePhpstanType(Model::class, random_int(0, 1) === 0 ? $model : $entity);
     }
 
     public function testVirtualInterfacesUnion(): void
     {
         $modelClass = random_int(0, 1) === 0 ? Female::class : Male::class;
         $model = new $modelClass();
-        $this->assertSamePhpstanType(Female::class . '|' . Male::class, $model);
-        $this->assertSamePhpstanType(Female::class . '|' . Male::class, get_class($model)::assertInstanceOf($model));
+        static::assertSamePhpstanType(Female::class . '|' . Male::class, $model);
+        static::assertSamePhpstanType(Female::class . '|' . Male::class, get_class($model)::assertInstanceOf($model));
 
         $entity = $model->createEntity();
-        $this->assertSamePhpstanType('(' . IsEntity::class . '&' . Female::class . ')|(' . IsEntity::class . '&' . Male::class . ')', $entity);
-        $this->assertSamePhpstanType(Female::class . '|' . Male::class, $entity->getModel());
+        static::assertSamePhpstanType('(' . IsEntity::class . '&' . Female::class . ')|(' . IsEntity::class . '&' . Male::class . ')', $entity);
+        static::assertSamePhpstanType(Female::class . '|' . Male::class, $entity->getModel());
 
         if (PhpstanUtil::alwaysFalseAnalyseOnly()) {
             $model->assertIsEntity();
-            $this->assertSamePhpstanType('(' . IsEntity::class . '&' . Female::class . ')|(' . IsEntity::class . '&' . Male::class . ')', $model);
+            static::assertSamePhpstanType('(' . IsEntity::class . '&' . Female::class . ')|(' . IsEntity::class . '&' . Male::class . ')', $model);
             $model->assertIsModel();
-            $this->assertSamePhpstanType(Female::class . '|' . Male::class, $model);
+            static::assertSamePhpstanType(Female::class . '|' . Male::class, $model);
 
             $entity = $model->load(1);
-            $this->assertSamePhpstanType('(' . IsLoaded::class . '&' . Female::class . ')|(' . IsLoaded::class . '&' . Male::class . ')', $entity);
+            static::assertSamePhpstanType('(' . IsLoaded::class . '&' . Female::class . ')|(' . IsLoaded::class . '&' . Male::class . ')', $entity);
             $entity->unload();
-            $this->assertSamePhpstanType('(' . IsEntity::class . '&' . Female::class . ')|(' . IsEntity::class . '&' . Male::class . ')', $entity);
+            static::assertSamePhpstanType('(' . IsEntity::class . '&' . Female::class . ')|(' . IsEntity::class . '&' . Male::class . ')', $entity);
             $entity->assertIsLoaded();
-            $this->assertSamePhpstanType('(' . IsLoaded::class . '&' . Female::class . ')|(' . IsLoaded::class . '&' . Male::class . ')', $entity);
+            static::assertSamePhpstanType('(' . IsLoaded::class . '&' . Female::class . ')|(' . IsLoaded::class . '&' . Male::class . ')', $entity);
             $entity->assertIsEntity();
-            $this->assertSamePhpstanType('(' . IsLoaded::class . '&' . Female::class . ')|(' . IsLoaded::class . '&' . Male::class . ')', $entity);
+            static::assertSamePhpstanType('(' . IsLoaded::class . '&' . Female::class . ')|(' . IsLoaded::class . '&' . Male::class . ')', $entity);
         }
     }
 }
