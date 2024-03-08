43530fdcf707a67baa125649cebc0312d39f0d1e
Michael Voříšek <mvorisek@mvorisek.cz>
2024-03-08 09:59:38 +0100

    rm no longer needed phpstan ignore

--- src/Persistence/Sql/Connection.php
+++ src/Persistence/Sql/Connection.php
@@ -83,8 +83,7 @@ abstract class Connection
         }
         if (isset($dsn['dsn'])) {
             if (str_contains($dsn['dsn'], '://')) {
-                /** @var array<string, string> https://github.com/phpstan/phpstan/issues/8638 */
-                $parsed = array_filter(parse_url($dsn['dsn'])); // @phpstan-ignore-line
+                $parsed = array_filter(parse_url($dsn['dsn']));
                 $dsn['dsn'] = str_replace('-', '_', $parsed['scheme']) . ':';
                 unset($parsed['scheme']);
                 foreach ($parsed as $k => $v) {
