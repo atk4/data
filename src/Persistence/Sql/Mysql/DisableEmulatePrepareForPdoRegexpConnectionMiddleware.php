b0af0d3b20d3c253fbfcc390a31215e4ef786101
Michael Voříšek <mvorisek@mvorisek.cz>
2024-04-25 11:48:33 +0200

    improve comment

--- src/Persistence/Sql/Mysql/DisableEmulatePrepareForPdoRegexpConnectionMiddleware.php
+++ src/Persistence/Sql/Mysql/DisableEmulatePrepareForPdoRegexpConnectionMiddleware.php
@@ -12,13 +12,13 @@ class DisableEmulatePrepareForPdoRegexpConnectionMiddleware extends AbstractConn
     #[\Override]
     public function prepare(string $sql): Statement
     {
-        // workaround SQLSTATE[HY000]: General error: 3995 Character set 'binary' cannot be used
-        // in conjunction with 'utf8mb4_0900_ai_ci' in call to regexp_like.
+        // workaround MySQL v8.0.22 and higher SQLSTATE[HY000]: General error: 3995 Character set 'binary'
+        // cannot be used in conjunction with 'utf8mb4_0900_ai_ci' in call to regexp_like.
         // https://github.com/mysql/mysql-server/blob/72136a6d15/sql/item_regexp_func.cc#L115-L120
         // https://dbfiddle.uk/9SA-omyF
         $pdo = $this->getNativeConnection();
         if ($pdo instanceof \PDO && $pdo->getAttribute(\PDO::ATTR_EMULATE_PREPARES)
-            && preg_match('~ regexp ~i', preg_replace('~' . Expression::QUOTED_TOKEN_REGEX . '~', '', $sql))
+            && preg_match('~\sregexp\s|(?<!\w)regexp_[a-z]+\(~i', preg_replace('~' . Expression::QUOTED_TOKEN_REGEX . '~', '', $sql))
         ) {
             $pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
             try {
