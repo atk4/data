708cf02957a70df78065cd685d3352a1e5df3fa9
Michael Voříšek <mvorisek@mvorisek.cz>
2025-09-09 00:02:30 +0200

    add "with clause" mixed persistence test

--- tests/ModelMixedPersistenceTest.php
+++ tests/ModelMixedPersistenceTest.php
@@ -7,6 +7,7 @@ namespace Atk4\Data\Tests;
 use Atk4\Data\Model;
 use Atk4\Data\Persistence;
 use Atk4\Data\Schema\TestCase;
+use Doctrine\DBAL\Platforms\OraclePlatform;
 
 class ModelMixedPersistenceTest extends TestCase
 {
@@ -25,14 +26,84 @@ class ModelMixedPersistenceTest extends TestCase
         $m = new Model($this->db, ['table' => $mArray]);
         $m->addField('name');
 
+        $fromClause = $this->getDatabasePlatform() instanceof OraclePlatform
+            ? ' from "DUAL"'
+            : '';
+
         $this->assertSameSql(
-            'select `id`, `name` from (select :a `id`, :b `name` union all select :c, :d) `_tm`',
+            'select `id`, `name` from (select :a `id`, :b `name`' . $fromClause . ' union all select :c, :d' . $fromClause . ') `_tm`',
             $m->action('select')->render()[0]
         );
 
         self::assertSameExportUnordered([
             ['id' => 10, 'name' => 'John'],
             ['id' => 20, 'name' => 'Peter'],
+        ], $m->export());
+
+        $john = $m->load(10);
+        $john->set('name', 'Johny');
+        $john->save();
+
+        self::assertSameExportUnordered([
+            ['id' => 10, 'name' => 'Johny'],
+            ['id' => 20, 'name' => 'Peter'],
+        ], $m->export());
+    }
+
+    public function testArrayWith(): void
+    {
+        $this->setDb([
+            'user' => [
+                10 => ['id' => 10, 'name' => 'John'],
+                20 => ['id' => 20, 'name' => 'Peter'],
+            ],
+        ]);
+
+        $mUser = new Model($this->db, ['table' => 'user']);
+        $mUser->addField('name');
+
+        $dbArray = new Persistence\Array_([
+            'config' => [
+                1 => ['id' => 1, 'user_id' => 10, 'path' => '/home/john', 'float' => 0.0],
+                ['id' => 2, 'user_id' => 20, 'path' => '/home/p', 'float' => 2.0],
+            ],
+        ]);
+
+        $mArray = new Model($dbArray, ['table' => 'config']);
+        $mArray->addField('userId', ['type' => 'bigint', 'actual' => 'user_id']);
+        $mArray->addField('path');
+        $mArray->addField('float', ['type' => 'float']);
+
+        $m = clone $mUser;
+        $m->addCteModel('config', $mArray);
+        $jConfig = $m->join('config.userId'); // TODO join using "name" field once non-ID join is supported
+        $jConfig->addField('path');
+        $jConfig->addField('float', ['type' => 'float']);
+
+        $fromClause = $this->getDatabasePlatform() instanceof OraclePlatform
+            ? ' from "DUAL"'
+            : '';
+
+        $this->assertSameSql(
+            'with `config` as (select :a `id`, :b `userId`, :c `path`, :d `float`' . $fromClause . ' union all select :e, :f, :g, :h' . $fromClause . ')' . "\n"
+                . 'select `user`.`id`, `user`.`name`, `_config`.`path`, `_config`.`float` from `user` inner join `config` `_config` on `_config`.`userId` = `user`.`id`',
+            $m->action('select')->render()[0]
+        );
+
+        self::markTestIncompleteOnMySQL5xPlatformAsWithClauseIsNotSupported();
+
+        self::assertSameExportUnordered([
+            ['id' => 10, 'name' => 'John', 'path' => '/home/john', 'float' => 0.0],
+            ['id' => 20, 'name' => 'Peter', 'path' => '/home/p', 'float' => 2.0],
+        ], $m->export());
+
+        $john = $m->load(10);
+        $john->set('path', 'new path');
+        $john->save();
+
+        self::assertSameExportUnordered([
+            ['id' => 10, 'name' => 'John', 'path' => 'new path', 'float' => 0.0],
+            ['id' => 20, 'name' => 'Peter', 'path' => '/home/p', 'float' => 2.0],
         ], $m->export());
     }
 }
