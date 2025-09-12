96b0ac7291ad4e86003688b54aa776f141134d91
Michael Voříšek <mvorisek@mvorisek.cz>
2025-09-13 01:07:39 +0200

    refactor MaterializedArrayAction to allow DB-specific render

--- tests/Persistence/Sql/MaterializedArrayActionTest.php
+++ tests/Persistence/Sql/MaterializedArrayActionTest.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Atk4\Data\Tests\Persistence\Sql;
 
 use Atk4\Data\Persistence\Array_\Action as ArrayAction;
+use Atk4\Data\Persistence\Sql\Exception;
 use Atk4\Data\Persistence\Sql\Expressionable;
 use Atk4\Data\Persistence\Sql\MaterializedArrayAction;
 use Atk4\Data\Schema\TestCase;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Platforms\OraclePlatform;
 use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Platforms\SQLitePlatform;
 use Doctrine\DBAL\Platforms\SQLServerPlatform;
 
 class MaterializedArrayActionTest extends TestCase
@@ -29,47 +32,95 @@ class MaterializedArrayActionTest extends TestCase
         return $render;
     }
 
-    public function testBasic(): void
+    public function testRenderZeroRows(): void
     {
         $action = new ArrayAction([], ['a', 'bar']);
         $query = new MaterializedArrayAction($action);
 
-        $fromClause = $this->getDatabasePlatform() instanceof OraclePlatform
-            ? ' from "DUAL"'
-            : '';
-        $fixParamNameFx = fn ($v) => $this->getDatabasePlatform() instanceof OraclePlatform
-            ? ':xxaaa' . substr($v, 1)
-            : $v;
-
-        if ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
-            self::assertSameSql('select :a `a`, :b `bar` limit 0 offset 0', $this->renderQuery($query)[0]);
+        $render = $this->renderQuery($query);
+        if ($this->getDatabasePlatform() instanceof SQLitePlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` limit 0, 0', $render[0]);
+            self::assertSame([':a' => null, ':b' => null], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` limit 0, 0', $render[0]);
+            self::assertSame([':a' => null, ':b' => null], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` limit 0 offset 0', $render[0]);
+            self::assertSame([':a' => null, ':b' => null], $render[1]);
         } elseif ($this->getDatabasePlatform() instanceof SQLServerPlatform) {
-            self::assertSameSql('select :a `a`, :b `bar` order by (select null) offset 9223372036854775807 rows fetch next 1 rows only', $this->renderQuery($query)[0]);
+            self::assertSameSql('select :a `a`, :b `bar` order by (select null) offset 9223372036854775807 rows fetch next 1 rows only', $render[0]);
+            self::assertSame([':a' => null, ':b' => null], $render[1]);
         } elseif ($this->getDatabasePlatform() instanceof OraclePlatform) {
-            self::assertSameSql('select :a `a`, :b `bar` from "DUAL" fetch next 0 rows only', $this->renderQuery($query)[0]);
+            self::assertSameSql('select :a `a`, :b `bar` from "DUAL" fetch next 0 rows only', $render[0]);
+            self::assertSame([':xxaaaa' => null, ':xxaaab' => null], $render[1]);
         } else {
-            self::assertSameSql('select :a `a`, :b `bar` limit 0, 0', $this->renderQuery($query)[0]);
+            self::assertSameSql('select :a `a`, :b `bar` limit 0, 0', $render[0]);
+            self::assertSame([':a' => null, ':b' => null], $render[1]);
         }
-        self::assertSame([
-            $fixParamNameFx(':a') => null,
-            $fixParamNameFx(':b') => null,
-        ], $this->renderQuery($query)[1]);
+    }
 
-        $action->generator = new \ArrayIterator([['a' => 1, 'bar' => 'u']]);
-        self::assertSameSql('select :a `a`, :b `bar`' . $fromClause, $this->renderQuery($query)[0]);
-        self::assertSame([
-            $fixParamNameFx(':a') => 1,
-            $fixParamNameFx(':b') => 'u',
-        ], $this->renderQuery($query)[1]);
+    public function testRenderOneRow(): void
+    {
+        $action = new ArrayAction([['a' => 1, 'bar' => 'u']], ['a', 'bar']);
+        $query = new MaterializedArrayAction($action);
 
-        $action->generator = new \ArrayIterator([['a' => 1, 'bar' => 'u'], ['a' => null, 'bar' => 'v']]);
-        self::assertSameSql('select :a `a`, :b `bar`' . $fromClause . ' union all select :c, :d' . $fromClause, $this->renderQuery($query)[0]);
-        self::assertSame([
-            $fixParamNameFx(':a') => 1,
-            $fixParamNameFx(':b') => 'u',
-            $fixParamNameFx(':c') => null,
-            $fixParamNameFx(':d') => 'v',
-        ], $this->renderQuery($query)[1]);
+        $render = $this->renderQuery($query);
+        if ($this->getDatabasePlatform() instanceof SQLitePlatform) {
+            self::assertSameSql('select :a `a`, :b `bar`', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar`', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar`', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof SQLServerPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar`', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof OraclePlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` from "DUAL"', $render[0]);
+            self::assertSame([':xxaaaa' => 1, ':xxaaab' => 'u'], $render[1]);
+        } else {
+            self::assertSameSql('select :a `a`, :b `bar`', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u'], $render[1]);
+        }
+    }
+
+    public function testRenderMultipleRows(): void
+    {
+        $action = new ArrayAction([['a' => 1, 'bar' => 'u'], ['a' => null, 'bar' => 'v']], ['a', 'bar']);
+        $query = new MaterializedArrayAction($action);
+
+        $render = $this->renderQuery($query);
+        if ($this->getDatabasePlatform() instanceof SQLitePlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` union all select :c, :d', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u', ':c' => null, ':d' => 'v'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` union all select :c, :d', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u', ':c' => null, ':d' => 'v'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` union all select :c, :d', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u', ':c' => null, ':d' => 'v'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof SQLServerPlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` union all select :c, :d', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u', ':c' => null, ':d' => 'v'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof OraclePlatform) {
+            self::assertSameSql('select :a `a`, :b `bar` from "DUAL" union all select :c, :d from "DUAL"', $render[0]);
+            self::assertSame([':xxaaaa' => 1, ':xxaaab' => 'u', ':xxaaac' => null, ':xxaaad' => 'v'], $render[1]);
+        } else {
+            self::assertSameSql('select :a `a`, :b `bar` union all select :c, :d', $render[0]);
+            self::assertSame([':a' => 1, ':b' => 'u', ':c' => null, ':d' => 'v'], $render[1]);
+        }
+    }
+
+    public function testColumnTypeMismatchException(): void
+    {
+        $action = new ArrayAction([['a' => 1], ['a' => '1']], ['a']);
+        $query = new MaterializedArrayAction($action);
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Column consists of more than one type');
+        $this->renderQuery($query);
     }
 
     public function testGetRowsUpdatedGenerator(): void
