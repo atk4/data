2820e5d8c8d9e551cc825de698bc45ddc9b67e92
Michael Voříšek <mvorisek@mvorisek.cz>
2025-09-14 14:50:12 +0200

    working mysql

--- tests/Persistence/Sql/MaterializedArrayActionTest.php
+++ tests/Persistence/Sql/MaterializedArrayActionTest.php
@@ -8,6 +8,7 @@ use Atk4\Data\Persistence\Array_\Action as ArrayAction;
 use Atk4\Data\Persistence\Sql\Exception;
 use Atk4\Data\Persistence\Sql\Expressionable;
 use Atk4\Data\Persistence\Sql\MaterializedArrayAction;
+use Atk4\Data\Persistence\Sql\Mysql\Connection as MysqlConnection;
 use Atk4\Data\Schema\TestCase;
 use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Platforms\OraclePlatform;
@@ -32,6 +33,13 @@ class MaterializedArrayActionTest extends TestCase
         return $render;
     }
 
+    private function isServerMysql5x(): bool
+    {
+        return $this->getDatabasePlatform() instanceof MySQLPlatform
+            && !MysqlConnection::isServerMariaDb($this->getConnection())
+            && version_compare($this->getConnection()->getServerVersion(), '6.0') < 0;
+    }
+
     public function testRenderZeroRows(): void
     {
         $action = new ArrayAction([], ['bool', 'int', 'float', 'string']);
@@ -40,9 +48,8 @@ class MaterializedArrayActionTest extends TestCase
         $render = $this->renderQuery($query);
         if ($this->getDatabasePlatform() instanceof SQLitePlatform) {
             self::assertSame([':a' => '[]'], $render[1]);
-        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform) {
-            self::assertSameSql('select :a `bool`, :b `int`, :c `float`, :d `string` limit 0, 0', $render[0]);
-            self::assertSame([':a' => null, ':b' => null, ':c' => null, ':d' => null], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform && !$this->isServerMysql5x()) {
+            self::assertSame([':a' => '[]'], $render[1]);
         } elseif ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
             self::assertSameSql('select :a `bool`, :b `int`, :c `float`, :d `string` limit 0 offset 0', $render[0]);
             self::assertSame([':a' => null, ':b' => null, ':c' => null, ':d' => null], $render[1]);
@@ -70,9 +77,8 @@ class MaterializedArrayActionTest extends TestCase
         $render = $this->renderQuery($query);
         if ($this->getDatabasePlatform() instanceof SQLitePlatform) {
             self::assertSame([':a' => '[[false,0,0.0,"Mark"]]'], $render[1]);
-        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform) {
-            self::assertSameSql('select :a `bool`, :b `int`, :c `float`, :d `string`', $render[0]);
-            self::assertSame([':a' => false, ':b' => 0, ':c' => 0.0, ':d' => 'Mark'], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform && !$this->isServerMysql5x()) {
+            self::assertSame([':a' => '[[false,0,0.0,"Mark"]]'], $render[1]);
         } elseif ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
             self::assertSameSql('select :a `bool`, :b `int`, :c `float`, :d `string`', $render[0]);
             self::assertSame([':a' => false, ':b' => 0, ':c' => 0.0, ':d' => 'Mark'], $render[1]);
@@ -104,9 +110,9 @@ class MaterializedArrayActionTest extends TestCase
         if ($this->getDatabasePlatform() instanceof SQLitePlatform) {
             self::assertSameSql('select json_extract(value, \'$[0]\') `bool`, json_extract(value, \'$[1]\') `int`, json_extract(value, \'$[2]\') `float`, json_extract(value, \'$[3]\') `string` from json_each(:a)', $render[0]);
             self::assertSame([':a' => '[[true,-9223372036854775808,-1.0e-20,""],[null,9223372036854775807,1.0123456789123e+50," foo\n"]]'], $render[1]);
-        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform) {
-            self::assertSameSql('select :a `bool`, :b `int`, :c `float`, :d `string` union all select :e, :f, :g, :h', $render[0]);
-            self::assertSame([':a' => true, ':b' => \PHP_INT_MIN, ':c' => -1e-20, ':d' => '', ':e' => null, ':f' => \PHP_INT_MAX, ':g' => 1.0123456789123e50, ':h' => ' foo' . "\n"], $render[1]);
+        } elseif ($this->getDatabasePlatform() instanceof MySQLPlatform && !$this->isServerMysql5x()) {
+            self::assertSameSql('select `c0` `bool`, `c1` `int`, `c2` `float`, `c3` `string` from json_table(:a, \'$[*]\' columns (`c0` TINYINT(1) path \'$[0]\', `c1` BIGINT path \'$[1]\', `c2` DOUBLE PRECISION path \'$[2]\', `c3` VARCHAR(255) COLLATE `utf8mb4_unicode_ci` path \'$[3]\')) `t`', $render[0]);
+            self::assertSame([':a' => '[[true,-9223372036854775808,-1.0e-20,""],[null,9223372036854775807,1.0123456789123e+50," foo\n"]]'], $render[1]);
         } elseif ($this->getDatabasePlatform() instanceof PostgreSQLPlatform) {
             self::assertSameSql('select :a `bool`, :b `int`, :c `float`, :d `string` union all select :e, :f, :g, :h', $render[0]);
             self::assertSame([':a' => true, ':b' => \PHP_INT_MIN, ':c' => -1e-20, ':d' => '', ':e' => null, ':f' => \PHP_INT_MAX, ':g' => 1.0123456789123e50, ':h' => ' foo' . "\n"], $render[1]);
