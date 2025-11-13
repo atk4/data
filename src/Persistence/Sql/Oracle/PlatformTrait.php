2f89332640e87b45a8afc2e7cbf65b163523bcff
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-13 14:26:48 +0100

    better ignore

--- src/Persistence/Sql/Oracle/PlatformTrait.php
+++ src/Persistence/Sql/Oracle/PlatformTrait.php
@@ -40,7 +40,7 @@ trait PlatformTrait
         $lengthEncodedAscii = ($column['length'] ?? 255) * 2 + strlen("atk4_binary\ru5f8mzx4vsm8g2c9\r" . hash('crc32b', ''));
         $column['length'] = intdiv($lengthEncodedAscii + 3, 4);
 
-        return $this->getVarcharTypeDeclarationSQL($column);
+        return $this->getVarcharTypeDeclarationSQL($column); // @phpstan-ignore method.deprecated
     }
 
     #[\Override]
