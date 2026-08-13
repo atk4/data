a895abe0196c55e7146c00ec265a6b415743776e
Imants Horsts <imants.horsts@inbox.lv>
2026-08-14 00:35:39 +0300

    test2

--- src/Persistence/Sql/Oracle/RetryConnectionMiddleware.php
+++ src/Persistence/Sql/Oracle/RetryConnectionMiddleware.php
@@ -46,7 +46,7 @@ class RetryConnectionMiddleware implements Middleware
                             throw $e;
                         }
 
-                        throw new \Exception('Ready for retrying!!!');
+                        throw new \Exception('Going to retry connection!!!');//.
 
                         $timeoutMs = min(
                             self::RETRY_INTERVAL_BASE_MS * (2 ** $attempt),
