4ca9b206af6dd1c74c0931929f18d35cc34237b5
Michael Voříšek <mvorisek@mvorisek.cz>
2026-09-02 10:41:00 +0200

    do not allow nested order

--- src/Model.php
+++ src/Model.php
@@ -1069,6 +1069,8 @@ class Model implements \IteratorAggregate
                 if (is_int($k)) {
                     if (is_array($v)) {
                         // format [field, direction]
+                        assert(array_keys($v) === [0] || array_keys($v) === [0, 1]);
+                        assert(!is_array($v[0]));
                         $this->setOrder(...$v);
                     } else {
                         // format "field"
